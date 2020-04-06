3609335cd279848743df4ab078ff6b950973308b
Alexandre Coderre-Chabot <alexandre.coderre.chabot@gmail.com>
2020-04-05 20:33:28 -0700

    Add license and copyright notices to files.

--- test/Main.cpp
+++ test/Main.cpp
@@ -1,3 +1,9 @@
+// Copyright 2020 Alexandre Coderre-Chabot
+// This file is part of PhysicalQuantities, a C++17 library of physical quantities, units of measure, and related concepts for scientific computation.
+// PhysicalQuantities is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// PhysicalQuantities is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+// You should have received a copy of the GNU Lesser General Public License along with PhysicalQuantities. If not, see <https://www.gnu.org/licenses/>.
+
 #include "../lib/Acceleration.hpp"
 #include "../lib/AngularSpeed.hpp"
 #include "../lib/DisplacementGradient.hpp"
