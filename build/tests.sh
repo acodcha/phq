f854a2f4e70765c1eac8fdce3c5093d2b023e2c7
Alexandre Coderre-Chabot <alexandre.coderre.chabot@gmail.com>
2020-04-07 22:53:40 -0700

    Move unit tests to separate folder.

--- build/tests.sh
+++ build/tests.sh
@@ -3,7 +3,7 @@ cd "${0%/*}"
 mkdir -p -m 755 files
 cd files
 echo "Running CMake..."
-cmake -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=../../test ../../
+cmake -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=../bin ../../
 echo "Running Make with $(nproc) processors..."
 make -j $(nproc)
 echo "Building complete."
