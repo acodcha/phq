64da53626d8eaebf203402268abd6d4d8c31e662
Alexandre Coderre-Chabot <alexandre.coderre.chabot@gmail.com>
2023-02-23 01:02:17 -0800

    Update mass rate unit and add tests.

--- include/PhQ/Unit/MassRate.hpp
+++ include/PhQ/Unit/MassRate.hpp
@@ -1,10 +1,20 @@
 // Copyright 2020 Alexandre Coderre-Chabot
-// This file is part of Physical Quantities (PhQ), a C++17 header-only library of physical quantities, physical models, and units of measure for scientific computation.
-// Physical Quantities is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-// Physical Quantities is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
-// You should have received a copy of the GNU Lesser General Public License along with Physical Quantities. If not, see <https://www.gnu.org/licenses/>.
+//
+// This file is part of Physical Quantities (PhQ), a C++ library of physical
+// quantities, physical models, and units of measure for scientific computation.
+//
+// Physical Quantities is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version. Physical Quantities is distributed in the hope
+// that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details. You should have received a
+// copy of the GNU Lesser General Public License along with Physical Quantities.
+// If not, see <https://www.gnu.org/licenses/>.
 
-#pragma once
+#ifndef PHYSICAL_QUANTITIES_INCLUDE_PHQ_UNIT_MASS_RATE_HPP
+#define PHYSICAL_QUANTITIES_INCLUDE_PHQ_UNIT_MASS_RATE_HPP
 
 #include "Base.hpp"
 
@@ -17,81 +27,140 @@ enum class MassRate : int_least8_t {
   GramPerSecond,
   SlugPerSecond,
   SlinchPerSecond,
-  PoundPerSecond
+  PoundPerSecond,
 };
 
-} // namespace Unit
+}  // namespace Unit
 
-template <> const std::map<Unit::MassRate, std::string> abbreviations<Unit::MassRate>{
-  {Unit::MassRate::KilogramPerSecond, "kg/s"},
-  {Unit::MassRate::GramPerSecond, "g/s"},
-  {Unit::MassRate::SlugPerSecond, "slug/s"},
-  {Unit::MassRate::SlinchPerSecond, "slinch/s"},
-  {Unit::MassRate::PoundPerSecond, "lbm/s"}
-};
+template <>
+inline constexpr const Unit::MassRate StandardUnit<Unit::MassRate>{
+    Unit::MassRate::KilogramPerSecond};
 
-template <> const std::unordered_map<std::string, Unit::MassRate> spellings<Unit::MassRate>{
-  {"kg/s", Unit::MassRate::KilogramPerSecond},
-  {"kg/sec", Unit::MassRate::KilogramPerSecond},
-  {"g/s", Unit::MassRate::GramPerSecond},
-  {"g/sec", Unit::MassRate::GramPerSecond},
-  {"slug/s", Unit::MassRate::SlugPerSecond},
-  {"slug/sec", Unit::MassRate::SlugPerSecond},
-  {"slinch/s", Unit::MassRate::SlinchPerSecond},
-  {"slinch/sec", Unit::MassRate::SlinchPerSecond},
-  {"lbm/s", Unit::MassRate::PoundPerSecond},
-  {"lbm/sec", Unit::MassRate::PoundPerSecond},
-  {"lb/s", Unit::MassRate::PoundPerSecond},
-  {"lb/sec", Unit::MassRate::PoundPerSecond}
-};
+template <>
+inline constexpr const Dimension::Set Dimensions<Unit::MassRate>{Dimension::Set{
+    Dimension::Time{-1}, Dimension::Length{0}, Dimension::Mass{1}}};
 
-template <> const std::map<System, Unit::MassRate> consistent_units<Unit::MassRate>{
-  {System::MetreKilogramSecondKelvin, Unit::MassRate::KilogramPerSecond},
-  {System::MillimetreGramSecondKelvin, Unit::MassRate::GramPerSecond},
-  {System::FootPoundSecondRankine, Unit::MassRate::SlugPerSecond},
-  {System::InchPoundSecondRankine, Unit::MassRate::SlinchPerSecond}
-};
+template <>
+inline const std::map<UnitSystem, Unit::MassRate>
+    ConsistentUnits<Unit::MassRate>{
+        {UnitSystem::MetreKilogramSecondKelvin,
+         Unit::MassRate::KilogramPerSecond},
+        {UnitSystem::MillimetreGramSecondKelvin, Unit::MassRate::GramPerSecond},
+        {UnitSystem::FootPoundSecondRankine, Unit::MassRate::SlugPerSecond},
+        {UnitSystem::InchPoundSecondRankine, Unit::MassRate::SlinchPerSecond},
+    };
 
-template <> constexpr const Unit::MassRate standard_unit<Unit::MassRate>{Unit::MassRate::KilogramPerSecond};
+template <>
+inline const std::map<Unit::MassRate, UnitSystem>
+    RelatedUnitSystems<Unit::MassRate>{
+        {Unit::MassRate::KilogramPerSecond,
+         UnitSystem::MetreKilogramSecondKelvin},
+        {Unit::MassRate::GramPerSecond, UnitSystem::MillimetreGramSecondKelvin},
+        {Unit::MassRate::SlugPerSecond, UnitSystem::FootPoundSecondRankine},
+        {Unit::MassRate::SlinchPerSecond, UnitSystem::InchPoundSecondRankine},
+    };
 
-template <> constexpr const Dimension::Set dimension<Unit::MassRate>{Dimension::Set{Dimension::Length{0}, Dimension::Mass{1}, Dimension::Time{-1}}};
+template <>
+inline const std::map<Unit::MassRate, std::string_view>
+    Abbreviations<Unit::MassRate>{
+        {Unit::MassRate::KilogramPerSecond, "kg/s"},
+        {Unit::MassRate::GramPerSecond, "g/s"},
+        {Unit::MassRate::SlugPerSecond, "slug/s"},
+        {Unit::MassRate::SlinchPerSecond, "slinch/s"},
+        {Unit::MassRate::PoundPerSecond, "lbm/s"},
+    };
 
-template <> const std::map<Unit::MassRate, std::map<Unit::MassRate, std::function<void(std::vector<double>&)>>> conversions<Unit::MassRate>{
-  {Unit::MassRate::KilogramPerSecond, {
-    {Unit::MassRate::KilogramPerSecond, [](std::vector<double>& values)->void{}},
-    {Unit::MassRate::GramPerSecond, [](std::vector<double>& values)->void{std::for_each(values.begin(), values.end(), [](double& value)->void{value *= 1000.0;});}},
-    {Unit::MassRate::SlugPerSecond, [](std::vector<double>& values)->void{std::for_each(values.begin(), values.end(), [](double& value)->void{value *= 0.3048 / (0.45359237 * 9.80665);});}},
-    {Unit::MassRate::SlinchPerSecond, [](std::vector<double>& values)->void{std::for_each(values.begin(), values.end(), [](double& value)->void{value *= 0.0254 / (0.45359237 * 9.80665);});}},
-    {Unit::MassRate::PoundPerSecond, [](std::vector<double>& values)->void{std::for_each(values.begin(), values.end(), [](double& value)->void{value /= 0.45359237;});}}
-  }},
-  {Unit::MassRate::GramPerSecond, {
-    {Unit::MassRate::KilogramPerSecond, [](std::vector<double>& values)->void{std::for_each(values.begin(), values.end(), [](double& value)->void{value *= 0.001;});}},
-    {Unit::MassRate::GramPerSecond, [](std::vector<double>& values)->void{}},
-    {Unit::MassRate::SlugPerSecond, [](std::vector<double>& values)->void{std::for_each(values.begin(), values.end(), [](double& value)->void{value *= 0.0003048 / (0.45359237 * 9.80665);});}},
-    {Unit::MassRate::SlinchPerSecond, [](std::vector<double>& values)->void{std::for_each(values.begin(), values.end(), [](double& value)->void{value *= 0.0000254 / (0.45359237 * 9.80665);});}},
-    {Unit::MassRate::PoundPerSecond, [](std::vector<double>& values)->void{std::for_each(values.begin(), values.end(), [](double& value)->void{value /= 453.59237;});}}
-  }},
-  {Unit::MassRate::SlugPerSecond, {
-    {Unit::MassRate::KilogramPerSecond, [](std::vector<double>& values)->void{std::for_each(values.begin(), values.end(), [](double& value)->void{value *= 0.45359237 * 9.80665 / 0.3048;});}},
-    {Unit::MassRate::GramPerSecond, [](std::vector<double>& values)->void{std::for_each(values.begin(), values.end(), [](double& value)->void{value *= 453.59237 * 9.80665 / 0.3048;});}},
-    {Unit::MassRate::SlugPerSecond, [](std::vector<double>& values)->void{}},
-    {Unit::MassRate::SlinchPerSecond, [](std::vector<double>& values)->void{std::for_each(values.begin(), values.end(), [](double& value)->void{value /= 12.0;});}},
-    {Unit::MassRate::PoundPerSecond, [](std::vector<double>& values)->void{std::for_each(values.begin(), values.end(), [](double& value)->void{value *= 9.80665 / 0.3048;});}}
-  }},
-  {Unit::MassRate::SlinchPerSecond, {
-    {Unit::MassRate::KilogramPerSecond, [](std::vector<double>& values)->void{std::for_each(values.begin(), values.end(), [](double& value)->void{value *= 0.45359237 * 9.80665 / 0.0254;});}},
-    {Unit::MassRate::GramPerSecond, [](std::vector<double>& values)->void{std::for_each(values.begin(), values.end(), [](double& value)->void{value *= 453.59237 * 9.80665 / 0.0254;});}},
-    {Unit::MassRate::SlugPerSecond, [](std::vector<double>& values)->void{std::for_each(values.begin(), values.end(), [](double& value)->void{value *= 12.0;});}},
-    {Unit::MassRate::SlinchPerSecond, [](std::vector<double>& values)->void{}},
-    {Unit::MassRate::PoundPerSecond, [](std::vector<double>& values)->void{std::for_each(values.begin(), values.end(), [](double& value)->void{value *= 9.80665 / 0.0254;});}}
-  }},
-  {Unit::MassRate::PoundPerSecond, {
-    {Unit::MassRate::KilogramPerSecond, [](std::vector<double>& values)->void{std::for_each(values.begin(), values.end(), [](double& value)->void{value *= 0.45359237;});}},
-    {Unit::MassRate::GramPerSecond, [](std::vector<double>& values)->void{std::for_each(values.begin(), values.end(), [](double& value)->void{value *= 453.59237;});}},
-    {Unit::MassRate::SlugPerSecond, [](std::vector<double>& values)->void{std::for_each(values.begin(), values.end(), [](double& value)->void{value *= 0.3048 / 9.80665;});}},
-    {Unit::MassRate::SlinchPerSecond, [](std::vector<double>& values)->void{std::for_each(values.begin(), values.end(), [](double& value)->void{value *= 0.0254 / 9.80665;});}},
-    {Unit::MassRate::SlinchPerSecond, [](std::vector<double>& values)->void{}}
-  }}
-};
+template <>
+inline const std::unordered_map<std::string_view, Unit::MassRate>
+    Spellings<Unit::MassRate>{
+        {"kg/s", Unit::MassRate::KilogramPerSecond},
+        {"kg/sec", Unit::MassRate::KilogramPerSecond},
+        {"g/s", Unit::MassRate::GramPerSecond},
+        {"g/sec", Unit::MassRate::GramPerSecond},
+        {"slug/s", Unit::MassRate::SlugPerSecond},
+        {"slug/sec", Unit::MassRate::SlugPerSecond},
+        {"slinch/s", Unit::MassRate::SlinchPerSecond},
+        {"slinch/sec", Unit::MassRate::SlinchPerSecond},
+        {"lbm/s", Unit::MassRate::PoundPerSecond},
+        {"lbm/sec", Unit::MassRate::PoundPerSecond},
+        {"lb/s", Unit::MassRate::PoundPerSecond},
+        {"lb/sec", Unit::MassRate::PoundPerSecond},
+    };
 
-} // namespace PhQ
+template <>
+inline const std::map<
+    Unit::MassRate,
+    std::function<void(double* const values, const std::size_t size)>>
+    ConversionsFromStandard<Unit::MassRate>{
+        {Unit::MassRate::KilogramPerSecond,
+         [](double* values, const std::size_t size) -> void {}},
+        {Unit::MassRate::GramPerSecond,
+         [](double* values, const std::size_t size) -> void {
+           const double* const end{values + size};
+           for (; values < end; ++values) {
+             *values *= 1000.0;
+           }
+         }},
+        {Unit::MassRate::SlugPerSecond,
+         [](double* values, const std::size_t size) -> void {
+           const double* const end{values + size};
+           for (; values < end; ++values) {
+             *values *= 0.3048 / (0.45359237 * 9.80665);
+           }
+         }},
+        {Unit::MassRate::SlinchPerSecond,
+         [](double* values, const std::size_t size) -> void {
+           const double* const end{values + size};
+           for (; values < end; ++values) {
+             *values *= 0.0254 / (0.45359237 * 9.80665);
+           }
+         }},
+        {Unit::MassRate::PoundPerSecond,
+         [](double* values, const std::size_t size) -> void {
+           const double* const end{values + size};
+           for (; values < end; ++values) {
+             *values /= 0.45359237;
+           }
+         }},
+    };
+
+template <>
+inline const std::map<
+    Unit::MassRate,
+    std::function<void(double* const values, const std::size_t size)>>
+    ConversionsToStandard<Unit::MassRate>{
+        {Unit::MassRate::KilogramPerSecond,
+         [](double* values, const std::size_t size) -> void {}},
+        {Unit::MassRate::GramPerSecond,
+         [](double* values, const std::size_t size) -> void {
+           const double* const end{values + size};
+           for (; values < end; ++values) {
+             *values *= 0.001;
+           }
+         }},
+        {Unit::MassRate::SlugPerSecond,
+         [](double* values, const std::size_t size) -> void {
+           const double* const end{values + size};
+           for (; values < end; ++values) {
+             *values *= 0.45359237 * 9.80665 / 0.3048;
+           }
+         }},
+        {Unit::MassRate::SlinchPerSecond,
+         [](double* values, const std::size_t size) -> void {
+           const double* const end{values + size};
+           for (; values < end; ++values) {
+             *values *= 0.45359237 * 9.80665 / 0.0254;
+           }
+         }},
+        {Unit::MassRate::PoundPerSecond,
+         [](double* values, const std::size_t size) -> void {
+           const double* const end{values + size};
+           for (; values < end; ++values) {
+             *values *= 0.45359237;
+           }
+         }},
+    };
+
+}  // namespace PhQ
+
+#endif  // PHYSICAL_QUANTITIES_INCLUDE_PHQ_UNIT_MASS_RATE_HPP
---
+++ test/Unit/MassRate.cpp
@@ -0,0 +1,141 @@
+// Copyright 2020 Alexandre Coderre-Chabot
+//
+// This file is part of Physical Quantities (PhQ), a C++ library of physical
+// quantities, physical models, and units of measure for scientific computation.
+//
+// Physical Quantities is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version. Physical Quantities is distributed in the hope
+// that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details. You should have received a
+// copy of the GNU Lesser General Public License along with Physical Quantities.
+// If not, see <https://www.gnu.org/licenses/>.
+
+#include "../../include/PhQ/Unit/MassRate.hpp"
+
+#include <gtest/gtest.h>
+
+#include "Base.hpp"
+
+namespace PhQ::Unit {
+
+namespace {
+
+constexpr std::array<MassRate, 5> Units = {
+    MassRate::KilogramPerSecond, MassRate::GramPerSecond,
+    MassRate::SlugPerSecond,     MassRate::SlinchPerSecond,
+    MassRate::PoundPerSecond,
+};
+
+TEST(UnitEnergyFlux, Abbreviation) {
+  EXPECT_EQ(Abbreviation(MassRate::KilogramPerSecond), "kg/s");
+  EXPECT_EQ(Abbreviation(MassRate::GramPerSecond), "g/s");
+  EXPECT_EQ(Abbreviation(MassRate::SlugPerSecond), "slug/s");
+  EXPECT_EQ(Abbreviation(MassRate::SlinchPerSecond), "slinch/s");
+  EXPECT_EQ(Abbreviation(MassRate::PoundPerSecond), "lbm/s");
+}
+
+TEST(UnitEnergyFlux, ConsistentUnit) {
+  EXPECT_EQ(ConsistentUnit<MassRate>(UnitSystem::MetreKilogramSecondKelvin),
+            MassRate::KilogramPerSecond);
+  EXPECT_EQ(ConsistentUnit<MassRate>(UnitSystem::MillimetreGramSecondKelvin),
+            MassRate::GramPerSecond);
+  EXPECT_EQ(ConsistentUnit<MassRate>(UnitSystem::FootPoundSecondRankine),
+            MassRate::SlugPerSecond);
+  EXPECT_EQ(ConsistentUnit<MassRate>(UnitSystem::InchPoundSecondRankine),
+            MassRate::SlinchPerSecond);
+}
+
+TEST(UnitEnergyFlux, ConvertFromStandard) {
+  constexpr double value{10.0};
+  EXPECT_DOUBLE_EQ(ConvertCopy(value, MassRate::KilogramPerSecond,
+                               MassRate::KilogramPerSecond),
+                   value);
+  EXPECT_DOUBLE_EQ(
+      ConvertCopy(value, MassRate::KilogramPerSecond, MassRate::GramPerSecond),
+      value * 1000.0);
+  EXPECT_DOUBLE_EQ(
+      ConvertCopy(value, MassRate::KilogramPerSecond, MassRate::SlugPerSecond),
+      value * 0.3048 / (0.45359237 * 9.80665));
+  EXPECT_DOUBLE_EQ(ConvertCopy(value, MassRate::KilogramPerSecond,
+                               MassRate::SlinchPerSecond),
+                   value * 0.0254 / (0.45359237 * 9.80665));
+  EXPECT_DOUBLE_EQ(
+      ConvertCopy(value, MassRate::KilogramPerSecond, MassRate::PoundPerSecond),
+      value / 0.45359237);
+}
+
+TEST(UnitEnergyFlux, ConvertToStandard) {
+  constexpr double value{10.0};
+  EXPECT_DOUBLE_EQ(ConvertCopy(value, MassRate::KilogramPerSecond,
+                               MassRate::KilogramPerSecond),
+                   value);
+  EXPECT_DOUBLE_EQ(
+      ConvertCopy(value, MassRate::GramPerSecond, MassRate::KilogramPerSecond),
+      value * 0.001);
+  EXPECT_DOUBLE_EQ(
+      ConvertCopy(value, MassRate::SlugPerSecond, MassRate::KilogramPerSecond),
+      value * 0.45359237 * 9.80665 / 0.3048);
+  EXPECT_DOUBLE_EQ(ConvertCopy(value, MassRate::SlinchPerSecond,
+                               MassRate::KilogramPerSecond),
+                   value * 0.45359237 * 9.80665 / 0.0254);
+  EXPECT_DOUBLE_EQ(
+      ConvertCopy(value, MassRate::PoundPerSecond, MassRate::KilogramPerSecond),
+      value * 0.45359237);
+}
+
+TEST(UnitEnergyFlux, ConvertVerification) {
+  double value{10.0};
+  std::array<double, 3> array{10.0, -20.0, 30.0};
+  std::vector<double> std_vector{10.0, -20.0, 30.0, -40.0};
+  Value::Vector value_vector{10.0, -20.0, 30.0};
+  Value::SymmetricDyad symdyad{10.0, -20.0, 30.0, -40.0, 50.0, -60.0};
+  Value::Dyad dyad{10.0, -20.0, 30.0, -40.0, 50.0, -60.0, 70.0, -80.0, 90.0};
+  for (const MassRate old_unit : Units) {
+    for (const MassRate new_unit : Units) {
+      Convert(value, old_unit, new_unit);
+      Convert(array, old_unit, new_unit);
+      Convert(std_vector, old_unit, new_unit);
+      Convert(value_vector, old_unit, new_unit);
+      Convert(symdyad, old_unit, new_unit);
+      Convert(dyad, old_unit, new_unit);
+    }
+  }
+}
+
+TEST(UnitEnergyFlux, DimensionSet) {
+  EXPECT_EQ(Dimensions<MassRate>,
+            Dimension::Set(Dimension::Time{-1}, Dimension::Length{0},
+                           Dimension::Mass{1}));
+}
+
+TEST(UnitEnergyFlux, Parse) {
+  EXPECT_EQ(Parse<MassRate>("Hello world!"), std::nullopt);
+  EXPECT_EQ(Parse<MassRate>("kg/s"), MassRate::KilogramPerSecond);
+  EXPECT_EQ(Parse<MassRate>("g/s"), MassRate::GramPerSecond);
+  EXPECT_EQ(Parse<MassRate>("slug/s"), MassRate::SlugPerSecond);
+  EXPECT_EQ(Parse<MassRate>("slinch/s"), MassRate::SlinchPerSecond);
+  EXPECT_EQ(Parse<MassRate>("lbm/s"), MassRate::PoundPerSecond);
+}
+
+TEST(UnitEnergyFlux, RelatedUnitSystem) {
+  EXPECT_EQ(RelatedUnitSystem(MassRate::KilogramPerSecond),
+            UnitSystem::MetreKilogramSecondKelvin);
+  EXPECT_EQ(RelatedUnitSystem(MassRate::GramPerSecond),
+            UnitSystem::MillimetreGramSecondKelvin);
+  EXPECT_EQ(RelatedUnitSystem(MassRate::SlugPerSecond),
+            UnitSystem::FootPoundSecondRankine);
+  EXPECT_EQ(RelatedUnitSystem(MassRate::SlinchPerSecond),
+            UnitSystem::InchPoundSecondRankine);
+  EXPECT_EQ(RelatedUnitSystem(MassRate::PoundPerSecond), std::nullopt);
+}
+
+TEST(UnitEnergyFlux, StandardUnit) {
+  EXPECT_EQ(StandardUnit<MassRate>, MassRate::KilogramPerSecond);
+}
+
+}  // namespace
+
+}  // namespace PhQ::Unit
