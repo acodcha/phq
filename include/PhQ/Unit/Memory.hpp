6455d9472cd2cf72f809328d9fe01e7ffe54331a
Alexandre Coderre-Chabot <alexandre.coderre.chabot@gmail.com>
2023-02-24 00:12:15 -0800

    Update memory unit.

--- include/PhQ/Unit/Memory.hpp
+++ include/PhQ/Unit/Memory.hpp
@@ -1,17 +1,20 @@
 // Copyright 2020 Alexandre Coderre-Chabot
-// This file is part of Physical Quantities (PhQ), a C++17 header-only library
-// of physical quantities, physical models, and units of measure for scientific
-// computation. Physical Quantities is free software: you can redistribute it
-// and/or modify it under the terms of the GNU Lesser General Public License as
-// published by the Free Software Foundation, either version 3 of the License,
-// or (at your option) any later version. Physical Quantities is distributed in
-// the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
-// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
-// the GNU Lesser General Public License for more details. You should have
-// received a copy of the GNU Lesser General Public License along with Physical
-// Quantities. If not, see <https://www.gnu.org/licenses/>.
+//
+// This file is part of Physical Quantities (PhQ), a C++ library of physical
+// quantities, physical models, and units of measure for scientific computation.
+//
+// Physical Quantities is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version. Physical Quantities is distributed in the hope
+// that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details. You should have received a
+// copy of the GNU Lesser General Public License along with Physical Quantities.
+// If not, see <https://www.gnu.org/licenses/>.
 
-#pragma once
+#ifndef PHYSICAL_QUANTITIES_INCLUDE_PHQ_UNIT_MEMORY_HPP
+#define PHYSICAL_QUANTITIES_INCLUDE_PHQ_UNIT_MEMORY_HPP
 
 #include "Base.hpp"
 
@@ -464,3 +467,5 @@ inline const std::map<Unit::Memory, std::function<void(double* const values,
     };
 
 }  // namespace PhQ
+
+#endif  // PHYSICAL_QUANTITIES_INCLUDE_PHQ_UNIT_MEMORY_HPP
