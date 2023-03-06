dc501438a7b440aeb761dd9ffede919fea1d8209
Alexandre Coderre-Chabot <alexandre.coderre.chabot@gmail.com>
2023-03-05 20:07:39 -0800

    Update volume rate quantity and add tests.

--- include/PhQ/VolumeRate.hpp
+++ include/PhQ/VolumeRate.hpp
@@ -1,123 +1,179 @@
 // Copyright 2020-2023 Alexandre Coderre-Chabot
-// This file is part of Physical Quantities (PhQ), a C++17 header-only library of physical quantities, physical models, and units of measure for scientific computation.
-// Physical Quantities is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-// Physical Quantities is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
-// You should have received a copy of the GNU Lesser General Public License along with Physical Quantities. If not, see <https://www.gnu.org/licenses/>.
+//
+// This file is part of Physical Quantities (PhQ), a C++ library of physical
+// quantities, physical models, and units of measure for scientific computation.
+//
+// Physical Quantities is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version. Physical Quantities is distributed in the hope
+// that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details. You should have received a
+// copy of the GNU Lesser General Public License along with Physical Quantities.
+// If not, see <https://www.gnu.org/licenses/>.
 
-#pragma once
+#ifndef PHYSICAL_QUANTITIES_INCLUDE_PHQ_VOLUME_RATE_HPP
+#define PHYSICAL_QUANTITIES_INCLUDE_PHQ_VOLUME_RATE_HPP
 
 #include "Frequency.hpp"
-#include "Volume.hpp"
 #include "Unit/VolumeRate.hpp"
+#include "Volume.hpp"
 
 namespace PhQ {
 
 class VolumeRate : public DimensionalScalarQuantity<Unit::VolumeRate> {
-
 public:
+  constexpr VolumeRate() noexcept
+      : DimensionalScalarQuantity<Unit::VolumeRate>() {}
 
-  constexpr VolumeRate() noexcept : DimensionalScalarQuantity<Unit::VolumeRate>() {}
+  VolumeRate(const double value, const Unit::VolumeRate unit) noexcept
+      : DimensionalScalarQuantity<Unit::VolumeRate>(value, unit) {}
 
-  VolumeRate(double value, Unit::VolumeRate unit) noexcept : DimensionalScalarQuantity<Unit::VolumeRate>(value, unit) {}
+  constexpr VolumeRate(const Volume& volume, const Time& time) noexcept
+      : VolumeRate(volume.Value() / time.Value()) {}
 
-  constexpr VolumeRate(const Volume& volume, const Time& time) noexcept : VolumeRate(volume.Value() / time.Value()) {}
+  constexpr VolumeRate(const Volume& volume,
+                       const Frequency& frequency) noexcept
+      : VolumeRate(volume.Value() * frequency.Value()) {}
 
-  constexpr VolumeRate(const Volume& volume, const Frequency& frequency) noexcept : VolumeRate(volume.Value() * frequency.Value()) {}
-
-  constexpr bool operator==(const VolumeRate& volume_rate) const noexcept {
-    return value_ == volume_rate.value_;
+  inline VolumeRate operator+(const VolumeRate& volume_rate) const noexcept {
+    return VolumeRate{value_ + volume_rate.value_};
   }
 
-  constexpr bool operator!=(const VolumeRate& volume_rate) const noexcept {
-    return value_ != volume_rate.value_;
+  inline VolumeRate operator-(const VolumeRate& volume_rate) const noexcept {
+    return VolumeRate{value_ - volume_rate.value_};
   }
 
-  constexpr bool operator<(const VolumeRate& volume_rate) const noexcept {
-    return value_ < volume_rate.value_;
+  inline VolumeRate operator*(const double number) const noexcept {
+    return VolumeRate{value_ * number};
   }
 
-  constexpr bool operator<=(const VolumeRate& volume_rate) const noexcept {
-    return value_ <= volume_rate.value_;
-  }
-
-  constexpr bool operator>(const VolumeRate& volume_rate) const noexcept {
-    return value_ > volume_rate.value_;
-  }
-
-  constexpr bool operator>=(const VolumeRate& volume_rate) const noexcept {
-    return value_ >= volume_rate.value_;
-  }
-
-  VolumeRate operator+(const VolumeRate& volume_rate) const noexcept {
-    return {value_ + volume_rate.value_};
-  }
-
-  constexpr void operator+=(const VolumeRate& volume_rate) noexcept {
-    value_ += volume_rate.value_;
-  }
-
-  VolumeRate operator-(const VolumeRate& volume_rate) const noexcept {
-    return {value_ - volume_rate.value_};
-  }
-
-  constexpr void operator-=(const VolumeRate& volume_rate) noexcept {
-    value_ -= volume_rate.value_;
-  }
-
-  Volume operator*(const Time& time) const noexcept {
+  inline Volume operator*(const Time& time) const noexcept {
     return {*this, time};
   }
 
-  Volume operator/(const Frequency& frequency) const noexcept {
+  inline VolumeRate operator/(const double number) const noexcept {
+    return VolumeRate{value_ / number};
+  }
+
+  inline Volume operator/(const Frequency& frequency) const noexcept {
     return {*this, frequency};
   }
 
-  Frequency operator/(const Volume& volume) const noexcept {
+  inline Frequency operator/(const Volume& volume) const noexcept {
     return {*this, volume};
   }
 
+  inline constexpr void operator+=(const VolumeRate& volume_rate) noexcept {
+    value_ += volume_rate.value_;
+  }
+
+  inline constexpr void operator-=(const VolumeRate& volume_rate) noexcept {
+    value_ -= volume_rate.value_;
+  }
+
+  inline constexpr void operator*=(const double number) noexcept {
+    value_ *= number;
+  }
+
+  inline constexpr void operator/=(const double number) noexcept {
+    value_ /= number;
+  }
+
 private:
-
-  constexpr VolumeRate(double value) noexcept : DimensionalScalarQuantity<Unit::VolumeRate>(value) {}
-
+  explicit constexpr VolumeRate(const double value) noexcept
+      : DimensionalScalarQuantity<Unit::VolumeRate>(value) {}
 };
 
-constexpr Volume::Volume(const VolumeRate& volume_rate, const Time& time) noexcept : Volume(volume_rate.Value() * time.Value()) {}
+inline constexpr bool operator==(const VolumeRate& left,
+                                 const VolumeRate& right) noexcept {
+  return left.Value() == right.Value();
+}
 
-constexpr Volume::Volume(const VolumeRate& volume_rate, const Frequency& frequency) noexcept : Volume(volume_rate.Value() / frequency.Value()) {}
+inline constexpr bool operator!=(const VolumeRate& left,
+                                 const VolumeRate& right) noexcept {
+  return left.Value() != right.Value();
+}
 
-constexpr Time::Time(const VolumeRate& volume_rate, const Volume& volume) noexcept : Time(volume.Value() / volume_rate.Value()) {}
+inline constexpr bool operator<(const VolumeRate& left,
+                                const VolumeRate& right) noexcept {
+  return left.Value() < right.Value();
+}
 
-constexpr Frequency::Frequency(const VolumeRate& volume_rate, const Volume& volume) noexcept : Frequency(volume_rate.Value() / volume.Value()) {}
+inline constexpr bool operator>(const VolumeRate& left,
+                                const VolumeRate& right) noexcept {
+  return left.Value() > right.Value();
+}
 
-Volume Time::operator*(const VolumeRate& volume_rate) const noexcept {
+inline constexpr bool operator<=(const VolumeRate& left,
+                                 const VolumeRate& right) noexcept {
+  return left.Value() <= right.Value();
+}
+
+inline constexpr bool operator>=(const VolumeRate& left,
+                                 const VolumeRate& right) noexcept {
+  return left.Value() >= right.Value();
+}
+
+inline std::ostream& operator<<(std::ostream& stream,
+                                const VolumeRate& volume_rate) noexcept {
+  stream << volume_rate.Print();
+  return stream;
+}
+
+inline VolumeRate operator*(const double number,
+                            const VolumeRate& volume_rate) noexcept {
+  return volume_rate * number;
+}
+
+inline constexpr Volume::Volume(const VolumeRate& volume_rate,
+                                const Time& time) noexcept
+    : Volume(volume_rate.Value() * time.Value()) {}
+
+inline constexpr Volume::Volume(const VolumeRate& volume_rate,
+                                const Frequency& frequency) noexcept
+    : Volume(volume_rate.Value() / frequency.Value()) {}
+
+inline constexpr Time::Time(const VolumeRate& volume_rate,
+                            const Volume& volume) noexcept
+    : Time(volume.Value() / volume_rate.Value()) {}
+
+inline constexpr Frequency::Frequency(const VolumeRate& volume_rate,
+                                      const Volume& volume) noexcept
+    : Frequency(volume_rate.Value() / volume.Value()) {}
+
+inline Volume Time::operator*(const VolumeRate& volume_rate) const noexcept {
   return {volume_rate, *this};
 }
 
-VolumeRate Volume::operator*(const Frequency& frequency) const noexcept {
+inline VolumeRate Volume::operator*(const Frequency& frequency) const noexcept {
   return {*this, frequency};
 }
 
-Time Volume::operator/(const VolumeRate& volume_rate) const noexcept {
+inline Time Volume::operator/(const VolumeRate& volume_rate) const noexcept {
   return {volume_rate, *this};
 }
 
-VolumeRate Frequency::operator*(const Volume& volume) const noexcept {
+inline VolumeRate Frequency::operator*(const Volume& volume) const noexcept {
   return {volume, *this};
 }
 
-VolumeRate Volume::operator/(const Time& time) const noexcept {
+inline VolumeRate Volume::operator/(const Time& time) const noexcept {
   return {*this, time};
 }
 
-} // namespace PhQ
+}  // namespace PhQ
 
 namespace std {
 
-template <> struct hash<PhQ::VolumeRate> {
+template <>
+struct hash<PhQ::VolumeRate> {
   size_t operator()(const PhQ::VolumeRate& volume_rate) const {
     return hash<double>()(volume_rate.Value());
   }
 };
 
-} // namespace std
+}  // namespace std
+
+#endif  // PHYSICAL_QUANTITIES_INCLUDE_PHQ_VOLUME_RATE_HPP
