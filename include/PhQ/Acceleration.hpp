e24fed531cdf0d9a384051a825a3cb1b8d90d265
Alexandre Coderre-Chabot <alexandre.coderre.chabot@gmail.com>
2023-03-05 19:30:24 -0800

    Update acceleration quantity and add tests.

--- include/PhQ/Acceleration.hpp
+++ include/PhQ/Acceleration.hpp
@@ -1,10 +1,20 @@
 // Copyright 2020-2023 Alexandre Coderre-Chabot
-// This file is part of Physical Quantities (PhQ), a C++17 header-only library of physical quantities, physical models, and units of measure for scientific computation.
-// Physical Quantities is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-// Physical Quantities is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
-// You should have received a copy of the GNU Lesser General Public License along with Physical Quantities. If not, see <https://www.gnu.org/licenses/>.
+//
+// This file is part of Physical Quantities (PhQ), a C++ library of physical
+// quantities, physical models, and units of measure for scientific computation.
+//
+// Physical Quantities is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version. Physical Quantities is distributed in the hope
+// that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details. You should have received a
+// copy of the GNU Lesser General Public License along with Physical Quantities.
+// If not, see <https://www.gnu.org/licenses/>.
 
-#pragma once
+#ifndef PHYSICAL_QUANTITIES_INCLUDE_PHQ_ACCELERATION_HPP
+#define PHYSICAL_QUANTITIES_INCLUDE_PHQ_ACCELERATION_HPP
 
 #include "AccelerationMagnitude.hpp"
 #include "Velocity.hpp"
@@ -12,103 +22,153 @@
 namespace PhQ {
 
 class Acceleration : public DimensionalVectorQuantity<Unit::Acceleration> {
-
 public:
+  constexpr Acceleration() noexcept
+      : DimensionalVectorQuantity<Unit::Acceleration>() {}
 
-  constexpr Acceleration() noexcept : DimensionalVectorQuantity<Unit::Acceleration>() {}
+  Acceleration(const Value::Vector& value,
+               const Unit::Acceleration unit) noexcept
+      : DimensionalVectorQuantity<Unit::Acceleration>(value, unit) {}
 
-  Acceleration(const Value::Vector& value, Unit::Acceleration unit) noexcept : DimensionalVectorQuantity<Unit::Acceleration>(value, unit) {}
+  constexpr Acceleration(const AccelerationMagnitude& acceleration_magnitude,
+                         const Direction& direction) noexcept
+      : Acceleration(acceleration_magnitude.Value() * direction.Value()) {}
 
-  constexpr Acceleration(const AccelerationMagnitude& acceleration_magnitude, const Direction& direction) noexcept : Acceleration(acceleration_magnitude.Value() * direction.Value()) {}
+  constexpr Acceleration(const Velocity& velocity, const Time& time) noexcept
+      : Acceleration(velocity.Value() / time.Value()) {}
 
-  constexpr Acceleration(const Velocity& velocity, const Time& time) noexcept : Acceleration(velocity.Value() / time.Value()) {}
+  constexpr Acceleration(const Velocity& velocity,
+                         const Frequency& frequency) noexcept
+      : Acceleration(velocity.Value() * frequency.Value()) {}
 
-  constexpr Acceleration(const Velocity& velocity, const Frequency& frequency) noexcept : Acceleration(velocity.Value() * frequency.Value()) {}
+  inline AccelerationMagnitude Magnitude() const noexcept { return {*this}; }
 
-  AccelerationMagnitude Magnitude() const noexcept {
-    return {*this};
-  }
-
-  PhQ::Angle Angle(const Acceleration& acceleration) const noexcept {
+  inline PhQ::Angle Angle(const Acceleration& acceleration) const noexcept {
     return {*this, acceleration};
   }
 
-  constexpr bool operator==(const Acceleration& acceleration) const noexcept {
-    return value_ == acceleration.value_;
+  inline Acceleration operator+(
+      const Acceleration& acceleration) const noexcept {
+    return Acceleration{value_ + acceleration.value_};
   }
 
-  constexpr bool operator!=(const Acceleration& acceleration) const noexcept {
-    return value_ != acceleration.value_;
+  inline Acceleration operator-(
+      const Acceleration& acceleration) const noexcept {
+    return Acceleration{value_ - acceleration.value_};
   }
 
-  Acceleration operator+(const Acceleration& acceleration) const noexcept {
-    return {value_ + acceleration.value_};
+  inline Acceleration operator*(const double number) const noexcept {
+    return Acceleration{value_ * number};
   }
 
-  constexpr void operator+=(const Acceleration& acceleration) noexcept {
-    value_ += acceleration.value_;
-  }
-
-  Acceleration operator-(const Acceleration& acceleration) const noexcept {
-    return {value_ - acceleration.value_};
-  }
-
-  constexpr void operator-=(const Acceleration& acceleration) noexcept {
-    value_ -= acceleration.value_;
-  }
-
-  Velocity operator*(const Time& time) const noexcept {
+  inline Velocity operator*(const Time& time) const noexcept {
     return {*this, time};
   }
 
-  Velocity operator/(const Frequency& frequency) const noexcept {
+  inline Acceleration operator/(const double number) const noexcept {
+    return Acceleration{value_ / number};
+  }
+
+  inline Velocity operator/(const Frequency& frequency) const noexcept {
     return {*this, frequency};
   }
 
+  inline constexpr void operator+=(const Acceleration& acceleration) noexcept {
+    value_ += acceleration.value_;
+  }
+
+  inline constexpr void operator-=(const Acceleration& acceleration) noexcept {
+    value_ -= acceleration.value_;
+  }
+
+  inline constexpr void operator*=(const double number) noexcept {
+    value_ *= number;
+  }
+
+  inline constexpr void operator/=(const double number) noexcept {
+    value_ /= number;
+  }
+
 private:
-
-  constexpr Acceleration(const Value::Vector& value) noexcept : DimensionalVectorQuantity<Unit::Acceleration>(value) {}
-
+  explicit constexpr Acceleration(const Value::Vector& value) noexcept
+      : DimensionalVectorQuantity<Unit::Acceleration>(value) {}
 };
 
-Direction::Direction(const Acceleration& acceleration) noexcept : Direction(acceleration.Value()) {}
+inline constexpr bool operator==(const Acceleration& left,
+                                 const Acceleration& right) noexcept {
+  return left.Value() == right.Value();
+}
 
-Angle::Angle(const Acceleration& acceleration_1, const Acceleration& acceleration_2) noexcept : Angle(acceleration_1.Value(), acceleration_2.Value()) {}
+inline constexpr bool operator!=(const Acceleration& left,
+                                 const Acceleration& right) noexcept {
+  return left.Value() != right.Value();
+}
 
-AccelerationMagnitude::AccelerationMagnitude(const Acceleration& acceleration) noexcept : AccelerationMagnitude(acceleration.Value().Magnitude()) {}
+inline std::ostream& operator<<(std::ostream& stream,
+                                const Acceleration& acceleration) noexcept {
+  stream << acceleration.Print();
+  return stream;
+}
 
-constexpr Velocity::Velocity(const Acceleration& acceleration, const Time& time) noexcept : Velocity(acceleration.Value() * time.Value()) {}
+inline Acceleration operator*(const double number,
+                              const Acceleration& acceleration) noexcept {
+  return acceleration * number;
+}
 
-constexpr Velocity::Velocity(const Acceleration& acceleration, const Frequency& frequency) noexcept : Velocity(acceleration.Value() / frequency.Value()) {}
+inline Direction::Direction(const Acceleration& acceleration) noexcept
+    : Direction(acceleration.Value()) {}
 
-Acceleration Direction::operator*(const AccelerationMagnitude& acceleration_magnitude) const noexcept {
+inline Angle::Angle(const Acceleration& acceleration_1,
+                    const Acceleration& acceleration_2) noexcept
+    : Angle(acceleration_1.Value(), acceleration_2.Value()) {}
+
+inline AccelerationMagnitude::AccelerationMagnitude(
+    const Acceleration& acceleration) noexcept
+    : AccelerationMagnitude(acceleration.Value().Magnitude()) {}
+
+inline constexpr Velocity::Velocity(const Acceleration& acceleration,
+                                    const Time& time) noexcept
+    : Velocity(acceleration.Value() * time.Value()) {}
+
+inline constexpr Velocity::Velocity(const Acceleration& acceleration,
+                                    const Frequency& frequency) noexcept
+    : Velocity(acceleration.Value() / frequency.Value()) {}
+
+inline Acceleration Direction::operator*(
+    const AccelerationMagnitude& acceleration_magnitude) const noexcept {
   return {acceleration_magnitude, *this};
 }
 
-inline Velocity Time::operator*(const Acceleration& acceleration) const noexcept {
+inline Velocity Time::operator*(
+    const Acceleration& acceleration) const noexcept {
   return {acceleration, *this};
 }
 
-Acceleration AccelerationMagnitude::operator*(const Direction& direction) const noexcept {
+inline Acceleration AccelerationMagnitude::operator*(
+    const Direction& direction) const noexcept {
   return {*this, direction};
 }
 
-Acceleration Velocity::operator*(const Frequency& frequency) const noexcept {
+inline Acceleration Velocity::operator*(
+    const Frequency& frequency) const noexcept {
   return {*this, frequency};
 }
 
-Acceleration Velocity::operator/(const Time& time) const noexcept {
+inline Acceleration Velocity::operator/(const Time& time) const noexcept {
   return {*this, time};
 }
 
-} // namespace PhQ
+}  // namespace PhQ
 
 namespace std {
 
-template <> struct hash<PhQ::Acceleration> {
+template <>
+struct hash<PhQ::Acceleration> {
   size_t operator()(const PhQ::Acceleration& acceleration) const {
     return hash<PhQ::Value::Vector>()(acceleration.Value());
   }
 };
 
-} // namespace std
+}  // namespace std
+
+#endif  // PHYSICAL_QUANTITIES_INCLUDE_PHQ_ACCELERATION_HPP
---
+++ test/Acceleration.cpp
@@ -0,0 +1,204 @@
+// Copyright 2020-2023 Alexandre Coderre-Chabot
+//
+// This file is part of Physical Quantities (PhQ), a C++ library of physical
+// quantities, physical models, and units of measure for scientific computation.
+//
+// Physical Quantities is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version. Physical Quantities is distributed in the hope
+// that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details. You should have received a
+// copy of the GNU Lesser General Public License along with Physical Quantities.
+// If not, see <https://www.gnu.org/licenses/>.
+
+#include "../include/PhQ/Acceleration.hpp"
+
+#include <gtest/gtest.h>
+
+#include <unordered_set>
+
+namespace PhQ {
+
+namespace {
+
+TEST(Acceleration, Accessor) {
+  const Acceleration acceleration{{1.0, 2.0, 4.0},
+                                  Unit::Acceleration::MetrePerSquareSecond};
+  EXPECT_DOUBLE_EQ(acceleration.Value().x(), 1.0);
+  EXPECT_DOUBLE_EQ(acceleration.Value().y(), 2.0);
+  EXPECT_DOUBLE_EQ(acceleration.Value().z(), 4.0);
+  EXPECT_DOUBLE_EQ(
+      acceleration.Value(Unit::Acceleration::FootPerSquareSecond).x(),
+      1.0 / 0.3048);
+  EXPECT_DOUBLE_EQ(
+      acceleration.Value(Unit::Acceleration::FootPerSquareSecond).y(),
+      2.0 / 0.3048);
+  EXPECT_DOUBLE_EQ(
+      acceleration.Value(Unit::Acceleration::FootPerSquareSecond).z(),
+      4.0 / 0.3048);
+}
+
+TEST(Acceleration, AngleAndMagnitude) {
+  const Acceleration acceleration0{{0.0, 2.0, 0.0},
+                                   Unit::Acceleration::MetrePerSquareSecond};
+  const Acceleration acceleration1{{0.0, 0.0, 4.0},
+                                   Unit::Acceleration::MetrePerSquareSecond};
+  EXPECT_DOUBLE_EQ(
+      acceleration0.Angle(acceleration1).Value(Unit::Angle::Degree), 90.0);
+  EXPECT_DOUBLE_EQ(acceleration0.Magnitude().Value(), 2.0);
+}
+
+TEST(Acceleration, Arithmetic) {
+  const AccelerationMagnitude magnitude{
+      2.0, Unit::Acceleration::MetrePerSquareSecond};
+  const Direction direction{0.0, -1.0, 0.0};
+  const Time time{0.5, Unit::Time::Second};
+  const Frequency frequency{2.0, Unit::Frequency::Hertz};
+  const Velocity velocity{{0.5, 1.0, 2.0}, Unit::Speed::MetrePerSecond};
+  const Acceleration acceleration0{{0.0, 0.0, 0.0},
+                                   Unit::Acceleration::MetrePerSquareSecond};
+  const Acceleration acceleration1{{1.0, 2.0, 4.0},
+                                   Unit::Acceleration::MetrePerSquareSecond};
+  const Acceleration acceleration2{{2.0, 4.0, 8.0},
+                                   Unit::Acceleration::MetrePerSquareSecond};
+  EXPECT_EQ(acceleration1 + acceleration1, acceleration2);
+  EXPECT_EQ(acceleration1 - acceleration1, acceleration0);
+  EXPECT_EQ(acceleration1 * 2.0, acceleration2);
+  EXPECT_EQ(2.0 * acceleration1, acceleration2);
+  EXPECT_EQ(
+      direction * magnitude,
+      Acceleration({0.0, -2.0, 0.0}, Unit::Acceleration::MetrePerSquareSecond));
+  EXPECT_EQ(
+      magnitude * direction,
+      Acceleration({0.0, -2.0, 0.0}, Unit::Acceleration::MetrePerSquareSecond));
+  EXPECT_EQ(acceleration1 * time, velocity);
+  EXPECT_EQ(time * acceleration1, velocity);
+  EXPECT_EQ(velocity * frequency, acceleration1);
+  EXPECT_EQ(acceleration2 / 2.0, acceleration1);
+  EXPECT_EQ(acceleration1 / frequency, velocity);
+  EXPECT_EQ(velocity / time, acceleration1);
+
+  Acceleration acceleration3{acceleration1};
+  acceleration3 += acceleration1;
+  EXPECT_EQ(acceleration3, acceleration2);
+
+  Acceleration acceleration4{acceleration1};
+  acceleration4 -= acceleration1;
+  EXPECT_EQ(acceleration4, acceleration0);
+
+  Acceleration acceleration5{acceleration1};
+  acceleration5 *= 2.0;
+  EXPECT_EQ(acceleration5, acceleration2);
+
+  Acceleration acceleration6{acceleration2};
+  acceleration6 /= 2.0;
+  EXPECT_EQ(acceleration6, acceleration1);
+}
+
+TEST(Acceleration, Comparison) {
+  const Acceleration acceleration0{{1.0, 2.0, 4.0},
+                                   Unit::Acceleration::MetrePerSquareSecond};
+  const Acceleration acceleration1{{1.0, 2.0, -4.0},
+                                   Unit::Acceleration::MetrePerSquareSecond};
+  EXPECT_EQ(acceleration0, acceleration0);
+  EXPECT_NE(acceleration0, acceleration1);
+}
+
+TEST(Acceleration, Constructor) {
+  const Acceleration acceleration0;
+  const Acceleration acceleration1{{1.0, 2.0, 4.0},
+                                   Unit::Acceleration::FootPerSquareSecond};
+  const Acceleration acceleration2{{-1.0, -2.0, -4.0},
+                                   Unit::Acceleration::FootPerSquareSecond};
+  const Direction direction{acceleration1};
+  const Angle angle{acceleration1, acceleration2};
+  const AccelerationMagnitude magnitude{acceleration1};
+  const Time time{0.5, Unit::Time::Second};
+  const Frequency frequency{2.0, Unit::Frequency::Hertz};
+  const Velocity velocity0{acceleration1, time};
+  const Velocity velocity1{acceleration1, frequency};
+}
+
+TEST(Acceleration, Hash) {
+  const Acceleration acceleration0{{1.0, 2.0, 4.0},
+                                   Unit::Acceleration::FootPerSquareSecond};
+  const Acceleration acceleration1{{1.0, 2.000001, 4.0},
+                                   Unit::Acceleration::FootPerSquareSecond};
+  const Acceleration acceleration2{{1.0, 2.0, 5.0},
+                                   Unit::Acceleration::FootPerSquareSecond};
+  const Acceleration acceleration3{{1.0, 2.0, -4.0},
+                                   Unit::Acceleration::FootPerSquareSecond};
+  const Acceleration acceleration4{{1000000.0, 2000000.0, 4000000.0},
+                                   Unit::Acceleration::FootPerSquareSecond};
+  const Acceleration acceleration5{{-1.0, -2.0, -4.0},
+                                   Unit::Acceleration::FootPerSquareSecond};
+  const std::hash<Acceleration> hasher;
+  EXPECT_NE(hasher(acceleration0), hasher(acceleration1));
+  EXPECT_NE(hasher(acceleration0), hasher(acceleration2));
+  EXPECT_NE(hasher(acceleration0), hasher(acceleration3));
+  EXPECT_NE(hasher(acceleration0), hasher(acceleration4));
+  EXPECT_NE(hasher(acceleration0), hasher(acceleration5));
+  const std::unordered_set<Acceleration> unordered{
+      acceleration0, acceleration1, acceleration2,
+      acceleration3, acceleration4, acceleration5};
+}
+
+TEST(Acceleration, Json) {
+  EXPECT_EQ(
+      Acceleration({1.11, 2.22, 4.44}, Unit::Acceleration::MetrePerSquareSecond)
+          .Json(),
+      "{\"value\":{\"x\":1.110000,\"y\":2.220000,\"z\":4.440000},"
+      "\"unit\":\"m/s^2\"}");
+  EXPECT_EQ(
+      Acceleration({0.0, -5.0, 0.0}, Unit::Acceleration::FootPerSquareSecond)
+          .Json(Unit::Acceleration::FootPerSquareSecond),
+      "{\"value\":{\"x\":0,\"y\":-5.000000,\"z\":0},\"unit\":\"ft/s^2\"}");
+}
+
+TEST(Acceleration, Print) {
+  EXPECT_EQ(
+      Acceleration({1.11, 2.22, 4.44}, Unit::Acceleration::MetrePerSquareSecond)
+          .Print(),
+      "(1.110000, 2.220000, 4.440000) m/s^2");
+  EXPECT_EQ(
+      Acceleration({0.0, -5.0, 0.0}, Unit::Acceleration::FootPerSquareSecond)
+          .Print(Unit::Acceleration::FootPerSquareSecond),
+      "(0, -5.000000, 0) ft/s^2");
+}
+
+TEST(Acceleration, Stream) {
+  const Acceleration acceleration{{1.11, 2.22, 4.44},
+                                  Unit::Acceleration::MetrePerSquareSecond};
+  std::ostringstream stream;
+  stream << acceleration;
+  EXPECT_EQ(stream.str(), acceleration.Print());
+}
+
+TEST(Acceleration, Xml) {
+  EXPECT_EQ(
+      Acceleration({1.11, 2.22, 4.44}, Unit::Acceleration::MetrePerSquareSecond)
+          .Xml(),
+      "<value><x>1.110000</x><y>2.220000</y><z>4.440000</z></"
+      "value><unit>m/s^2</unit>");
+  EXPECT_EQ(
+      Acceleration({0.0, -5.0, 0.0}, Unit::Acceleration::FootPerSquareSecond)
+          .Xml(Unit::Acceleration::FootPerSquareSecond),
+      "<value><x>0</x><y>-5.000000</y><z>0</z></value><unit>ft/s^2</unit>");
+}
+
+TEST(Acceleration, Yaml) {
+  EXPECT_EQ(
+      Acceleration({1.11, 2.22, 4.44}, Unit::Acceleration::MetrePerSquareSecond)
+          .Yaml(),
+      "{value:{x:1.110000,y:2.220000,z:4.440000},unit:\"m/s^2\"}");
+  EXPECT_EQ(
+      Acceleration({0.0, -5.0, 0.0}, Unit::Acceleration::FootPerSquareSecond)
+          .Yaml(Unit::Acceleration::FootPerSquareSecond),
+      "{value:{x:0,y:-5.000000,z:0},unit:\"ft/s^2\"}");
+}
+
+}  // namespace
+
+}  // namespace PhQ
