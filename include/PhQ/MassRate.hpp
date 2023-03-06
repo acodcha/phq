d3cae04c8afdd3f33b0c195e1d4876aa3cce8a14
Alexandre Coderre-Chabot <alexandre.coderre.chabot@gmail.com>
2023-03-06 00:11:49 -0800

    Update mass rate quantity and add tests.

--- include/PhQ/MassRate.hpp
+++ include/PhQ/MassRate.hpp
@@ -1,10 +1,20 @@
 // Copyright 2020-2023 Alexandre Coderre-Chabot
-// This file is part of Physical Quantities (PhQ), a C++17 header-only library of physical quantities, physical models, and units of measure for scientific computation.
-// Physical Quantities is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-// Physical Quantities is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
-// You should have received a copy of the GNU Lesser General Public License along with Physical Quantities. If not, see <https://www.gnu.org/licenses/>.
+//
+// This file is part of Physical Quantities (PhQ), a C++ library of physical
+// quantities, physical models, and units of measure for scientific computation.
+//
+// Physical Quantities is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version. Physical Quantities is distributed in the hope
+// that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details. You should have received a
+// copy of the GNU Lesser General Public License along with Physical Quantities.
+// If not, see <https://www.gnu.org/licenses/>.
 
-#pragma once
+#ifndef PHYSICAL_QUANTITIES_INCLUDE_PHQ_MASS_RATE_HPP
+#define PHYSICAL_QUANTITIES_INCLUDE_PHQ_MASS_RATE_HPP
 
 #include "Frequency.hpp"
 #include "Mass.hpp"
@@ -13,111 +23,155 @@
 namespace PhQ {
 
 class MassRate : public DimensionalScalarQuantity<Unit::MassRate> {
-
 public:
-
   constexpr MassRate() noexcept : DimensionalScalarQuantity<Unit::MassRate>() {}
 
-  MassRate(double value, Unit::MassRate unit) noexcept : DimensionalScalarQuantity<Unit::MassRate>(value, unit) {}
+  MassRate(const double value, const Unit::MassRate unit) noexcept
+      : DimensionalScalarQuantity<Unit::MassRate>(value, unit) {}
 
-  constexpr MassRate(const Mass& mass, const Time& time) noexcept : MassRate(mass.Value() / time.Value()) {}
+  constexpr MassRate(const Mass& mass, const Time& time) noexcept
+      : MassRate(mass.Value() / time.Value()) {}
 
-  constexpr MassRate(const Mass& mass, const Frequency& frequency) noexcept : MassRate(mass.Value() * frequency.Value()) {}
+  constexpr MassRate(const Mass& mass, const Frequency& frequency) noexcept
+      : MassRate(mass.Value() * frequency.Value()) {}
 
-  constexpr bool operator==(const MassRate& mass_rate) const noexcept {
-    return value_ == mass_rate.value_;
+  inline MassRate operator+(const MassRate& mass_rate) const noexcept {
+    return MassRate{value_ + mass_rate.value_};
   }
 
-  constexpr bool operator!=(const MassRate& mass_rate) const noexcept {
-    return value_ != mass_rate.value_;
+  inline MassRate operator-(const MassRate& mass_rate) const noexcept {
+    return MassRate{value_ - mass_rate.value_};
   }
 
-  constexpr bool operator<(const MassRate& mass_rate) const noexcept {
-    return value_ < mass_rate.value_;
+  inline MassRate operator*(const double number) const noexcept {
+    return MassRate{value_ * number};
   }
 
-  constexpr bool operator<=(const MassRate& mass_rate) const noexcept {
-    return value_ <= mass_rate.value_;
-  }
-
-  constexpr bool operator>(const MassRate& mass_rate) const noexcept {
-    return value_ > mass_rate.value_;
-  }
-
-  constexpr bool operator>=(const MassRate& mass_rate) const noexcept {
-    return value_ >= mass_rate.value_;
-  }
-
-  MassRate operator+(const MassRate& mass_rate) const noexcept {
-    return {value_ + mass_rate.value_};
-  }
-
-  constexpr void operator+=(const MassRate& mass_rate) noexcept {
-    value_ += mass_rate.value_;
-  }
-
-  MassRate operator-(const MassRate& mass_rate) const noexcept {
-    return {value_ - mass_rate.value_};
-  }
-
-  constexpr void operator-=(const MassRate& mass_rate) noexcept {
-    value_ -= mass_rate.value_;
-  }
-
-  Frequency operator/(const Mass& mass) const noexcept {
-    return {*this, mass};
-  }
-
-  Mass operator*(const Time& time) const noexcept {
+  inline Mass operator*(const Time& time) const noexcept {
     return {*this, time};
   }
 
-  Mass operator/(const Frequency& frequency) const noexcept {
+  inline MassRate operator/(const double number) const noexcept {
+    return MassRate{value_ / number};
+  }
+
+  inline Frequency operator/(const Mass& mass) const noexcept {
+    return {*this, mass};
+  }
+
+  inline Mass operator/(const Frequency& frequency) const noexcept {
     return {*this, frequency};
   }
 
+  inline constexpr void operator+=(const MassRate& mass_rate) noexcept {
+    value_ += mass_rate.value_;
+  }
+
+  inline constexpr void operator-=(const MassRate& mass_rate) noexcept {
+    value_ -= mass_rate.value_;
+  }
+
+  inline constexpr void operator*=(const double number) noexcept {
+    value_ *= number;
+  }
+
+  inline constexpr void operator/=(const double number) noexcept {
+    value_ /= number;
+  }
+
 private:
-
-  constexpr MassRate(double value) noexcept : DimensionalScalarQuantity<Unit::MassRate>(value) {}
-
+  explicit constexpr MassRate(const double value) noexcept
+      : DimensionalScalarQuantity<Unit::MassRate>(value) {}
 };
 
-constexpr Time::Time(const MassRate& mass_rate, const Mass& mass) noexcept : Time(mass.Value() / mass_rate.Value()) {}
+inline constexpr bool operator==(const MassRate& left,
+                                 const MassRate& right) noexcept {
+  return left.Value() == right.Value();
+}
 
-constexpr Frequency::Frequency(const MassRate& mass_rate, const Mass& mass) noexcept : Frequency(mass_rate.Value() / mass.Value()) {}
+inline constexpr bool operator!=(const MassRate& left,
+                                 const MassRate& right) noexcept {
+  return left.Value() != right.Value();
+}
 
-constexpr Mass::Mass(const MassRate& mass_rate, const Time& time) noexcept : Mass(mass_rate.Value() * time.Value()) {}
+inline constexpr bool operator<(const MassRate& left,
+                                const MassRate& right) noexcept {
+  return left.Value() < right.Value();
+}
 
-constexpr Mass::Mass(const MassRate& mass_rate, const Frequency& frequency) noexcept : Mass(mass_rate.Value() / frequency.Value()) {}
+inline constexpr bool operator>(const MassRate& left,
+                                const MassRate& right) noexcept {
+  return left.Value() > right.Value();
+}
 
-Mass Time::operator*(const MassRate& mass_rate) const noexcept {
+inline constexpr bool operator<=(const MassRate& left,
+                                 const MassRate& right) noexcept {
+  return left.Value() <= right.Value();
+}
+
+inline constexpr bool operator>=(const MassRate& left,
+                                 const MassRate& right) noexcept {
+  return left.Value() >= right.Value();
+}
+
+inline std::ostream& operator<<(std::ostream& stream,
+                                const MassRate& mass_rate) noexcept {
+  stream << mass_rate.Print();
+  return stream;
+}
+
+inline MassRate operator*(const double number,
+                          const MassRate& mass_rate) noexcept {
+  return mass_rate * number;
+}
+
+inline constexpr Time::Time(const MassRate& mass_rate,
+                            const Mass& mass) noexcept
+    : Time(mass.Value() / mass_rate.Value()) {}
+
+inline constexpr Frequency::Frequency(const MassRate& mass_rate,
+                                      const Mass& mass) noexcept
+    : Frequency(mass_rate.Value() / mass.Value()) {}
+
+inline constexpr Mass::Mass(const MassRate& mass_rate,
+                            const Time& time) noexcept
+    : Mass(mass_rate.Value() * time.Value()) {}
+
+inline constexpr Mass::Mass(const MassRate& mass_rate,
+                            const Frequency& frequency) noexcept
+    : Mass(mass_rate.Value() / frequency.Value()) {}
+
+inline Mass Time::operator*(const MassRate& mass_rate) const noexcept {
   return {mass_rate, *this};
 }
 
-MassRate Mass::operator*(const Frequency& frequency) const noexcept {
+inline MassRate Mass::operator*(const Frequency& frequency) const noexcept {
   return {*this, frequency};
 }
 
-MassRate Frequency::operator*(const Mass& mass) const noexcept {
+inline MassRate Frequency::operator*(const Mass& mass) const noexcept {
   return {mass, *this};
 }
 
-MassRate Mass::operator/(const Time& time) const noexcept {
+inline MassRate Mass::operator/(const Time& time) const noexcept {
   return {*this, time};
 }
 
-Time Mass::operator/(const MassRate& mass_rate) const noexcept {
+inline Time Mass::operator/(const MassRate& mass_rate) const noexcept {
   return {mass_rate, *this};
 }
 
-} // namespace PhQ
+}  // namespace PhQ
 
 namespace std {
 
-template <> struct hash<PhQ::MassRate> {
+template <>
+struct hash<PhQ::MassRate> {
   size_t operator()(const PhQ::MassRate& mass_rate) const {
     return hash<double>()(mass_rate.Value());
   }
 };
 
-} // namespace std
+}  // namespace std
+
+#endif  // PHYSICAL_QUANTITIES_INCLUDE_PHQ_MASS_RATE_HPP
---
+++ test/MassRate.cpp
@@ -0,0 +1,151 @@
+// Copyright 2020-2023 Alexandre Coderre-Chabot
+//
+// This file is part of Physical Quantities (PhQ), a C++ library of physical
+// quantities, physical models, and units of measure for scientific computation.
+//
+// Physical Quantities is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version. Physical Quantities is distributed in the hope
+// that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details. You should have received a
+// copy of the GNU Lesser General Public License along with Physical Quantities.
+// If not, see <https://www.gnu.org/licenses/>.
+
+#include "../include/PhQ/MassRate.hpp"
+
+#include <gtest/gtest.h>
+
+#include <unordered_set>
+
+namespace PhQ {
+
+namespace {
+
+TEST(MassRate, Accessor) {
+  const MassRate rate{0.1, Unit::MassRate::KilogramPerSecond};
+  EXPECT_DOUBLE_EQ(rate.Value(), 0.1);
+  EXPECT_DOUBLE_EQ(rate.Value(Unit::MassRate::GramPerSecond), 0.1 * 1000.0);
+}
+
+TEST(MassRate, Arithmetic) {
+  const Time time{0.5, Unit::Time::Second};
+  const Frequency frequency{2.0, Unit::Frequency::Hertz};
+  const Mass mass{0.5, Unit::Mass::Kilogram};
+  const MassRate rate0{0.0, Unit::MassRate::KilogramPerSecond};
+  const MassRate rate1{1.0, Unit::MassRate::KilogramPerSecond};
+  const MassRate rate2{2.0, Unit::MassRate::KilogramPerSecond};
+  EXPECT_EQ(rate1 + rate1, rate2);
+  EXPECT_EQ(rate1 - rate1, rate0);
+  EXPECT_EQ(rate1 * 2.0, rate2);
+  EXPECT_EQ(2.0 * rate1, rate2);
+  EXPECT_EQ(rate1 * time, mass);
+  EXPECT_EQ(time * rate1, mass);
+  EXPECT_EQ(rate2 / 2.0, rate1);
+  EXPECT_EQ(rate1 / frequency, mass);
+  EXPECT_EQ(rate1 / mass, frequency);
+  EXPECT_EQ(mass / rate1, time);
+  EXPECT_EQ(mass / time, rate1);
+
+  MassRate rate3{rate1};
+  rate3 += rate1;
+  EXPECT_EQ(rate3, rate2);
+
+  MassRate rate4{rate1};
+  rate4 -= rate1;
+  EXPECT_EQ(rate4, rate0);
+
+  MassRate rate5{rate1};
+  rate5 *= 2.0;
+  EXPECT_EQ(rate5, rate2);
+
+  MassRate rate6{rate2};
+  rate6 /= 2.0;
+  EXPECT_EQ(rate6, rate1);
+}
+
+TEST(MassRate, Comparison) {
+  const MassRate rate0{0.1, Unit::MassRate::KilogramPerSecond};
+  const MassRate rate1{0.2, Unit::MassRate::KilogramPerSecond};
+  EXPECT_EQ(rate0, rate0);
+  EXPECT_NE(rate0, rate1);
+  EXPECT_LT(rate0, rate1);
+  EXPECT_GT(rate1, rate0);
+  EXPECT_LE(rate0, rate0);
+  EXPECT_LE(rate0, rate1);
+  EXPECT_GE(rate0, rate0);
+  EXPECT_GE(rate1, rate0);
+}
+
+TEST(MassRate, Constructor) {
+  const Time time0{0.5, Unit::Time::Second};
+  const Frequency frequency0{2.0, Unit::Frequency::Hertz};
+  const Mass mass0{0.5, Unit::Mass::Kilogram};
+  const MassRate rate0;
+  const MassRate rate1{100.0, Unit::MassRate::GramPerSecond};
+  const Mass mass1{rate1, time0};
+  const Mass mass2{rate1, frequency0};
+  const Time time1{rate1, mass0};
+  const Frequency frequency1{rate1, mass0};
+}
+
+TEST(MassRate, Hash) {
+  const MassRate rate0{10.0, Unit::MassRate::GramPerSecond};
+  const MassRate rate1{10.000001, Unit::MassRate::GramPerSecond};
+  const MassRate rate2{11.0, Unit::MassRate::GramPerSecond};
+  const MassRate rate3{-10.0, Unit::MassRate::GramPerSecond};
+  const MassRate rate4{20000.0, Unit::MassRate::GramPerSecond};
+  const MassRate rate5{-123.456, Unit::MassRate::GramPerSecond};
+  const std::hash<MassRate> hasher;
+  EXPECT_NE(hasher(rate0), hasher(rate1));
+  EXPECT_NE(hasher(rate0), hasher(rate2));
+  EXPECT_NE(hasher(rate0), hasher(rate3));
+  EXPECT_NE(hasher(rate0), hasher(rate4));
+  EXPECT_NE(hasher(rate0), hasher(rate5));
+  const std::unordered_set<MassRate> unordered{rate0, rate1, rate2,
+                                               rate3, rate4, rate5};
+}
+
+TEST(MassRate, Json) {
+  EXPECT_EQ(MassRate(1.11, Unit::MassRate::KilogramPerSecond).Json(),
+            "{\"value\":1.110000,\"unit\":\"kg/s\"}");
+  EXPECT_EQ(MassRate(-5.0, Unit::MassRate::GramPerSecond)
+                .Json(Unit::MassRate::GramPerSecond),
+            "{\"value\":-5.000000,\"unit\":\"g/s\"}");
+}
+
+TEST(MassRate, Print) {
+  EXPECT_EQ(MassRate(1.11, Unit::MassRate::KilogramPerSecond).Print(),
+            "1.110000 kg/s");
+  EXPECT_EQ(MassRate(-5.0, Unit::MassRate::GramPerSecond)
+                .Print(Unit::MassRate::GramPerSecond),
+            "-5.000000 g/s");
+}
+
+TEST(MassRate, Stream) {
+  const MassRate rate{1.11, Unit::MassRate::KilogramPerSecond};
+  std::ostringstream stream;
+  stream << rate;
+  EXPECT_EQ(stream.str(), rate.Print());
+}
+
+TEST(MassRate, Xml) {
+  EXPECT_EQ(MassRate(1.11, Unit::MassRate::KilogramPerSecond).Xml(),
+            "<value>1.110000</value><unit>kg/s</unit>");
+  EXPECT_EQ(MassRate(-5.0, Unit::MassRate::GramPerSecond)
+                .Xml(Unit::MassRate::GramPerSecond),
+            "<value>-5.000000</value><unit>g/s</unit>");
+}
+
+TEST(MassRate, Yaml) {
+  EXPECT_EQ(MassRate(1.11, Unit::MassRate::KilogramPerSecond).Yaml(),
+            "{value:1.110000,unit:\"kg/s\"}");
+  EXPECT_EQ(MassRate(-5.0, Unit::MassRate::GramPerSecond)
+                .Yaml(Unit::MassRate::GramPerSecond),
+            "{value:-5.000000,unit:\"g/s\"}");
+}
+
+}  // namespace
+
+}  // namespace PhQ
