d28a86da80f0ceae7aafff8552ca9eef29c9b585
Alexandre Coderre-Chabot <alexandre.coderre.chabot@gmail.com>
2023-03-06 17:17:56 -0800

    Update dynamic viscosity quantity and add tests.

--- include/PhQ/DynamicViscosity.hpp
+++ include/PhQ/DynamicViscosity.hpp
@@ -1,10 +1,20 @@
 // Copyright 2020-2023 Alexandre Coderre-Chabot
-// This file is part of Physical Quantities (PhQ), a C++17 header-only library of physical quantities, physical models, and units of measure for scientific computation.
-// Physical Quantities is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-// Physical Quantities is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
-// You should have received a copy of the GNU Lesser General Public License along with Physical Quantities. If not, see <https://www.gnu.org/licenses/>.
+//
+// This file is part of Physical Quantities (PhQ), a C++ library of physical
+// quantities, physical models, and units of measure for scientific computation.
+//
+// Physical Quantities is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version. Physical Quantities is distributed in the hope
+// that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details. You should have received a
+// copy of the GNU Lesser General Public License along with Physical Quantities.
+// If not, see <https://www.gnu.org/licenses/>.
 
-#pragma once
+#ifndef PHYSICAL_QUANTITIES_INCLUDE_PHQ_DYNAMIC_VISCOSITY_HPP
+#define PHYSICAL_QUANTITIES_INCLUDE_PHQ_DYNAMIC_VISCOSITY_HPP
 
 #include "KinematicViscosity.hpp"
 #include "MassDensity.hpp"
@@ -12,99 +22,157 @@
 
 namespace PhQ {
 
-// Forward declarations.
+// Forward declarations for class DynamicViscosity.
 class ReynoldsNumber;
 class ThermalConductivityScalar;
 class SpecificIsobaricHeatCapacity;
 
-class DynamicViscosity : public DimensionalScalarQuantity<Unit::DynamicViscosity> {
-
+class DynamicViscosity
+    : public DimensionalScalarQuantity<Unit::DynamicViscosity> {
 public:
+  constexpr DynamicViscosity() noexcept
+      : DimensionalScalarQuantity<Unit::DynamicViscosity>() {}
 
-  constexpr DynamicViscosity() noexcept : DimensionalScalarQuantity<Unit::DynamicViscosity>() {}
+  DynamicViscosity(const double value,
+                   const Unit::DynamicViscosity unit) noexcept
+      : DimensionalScalarQuantity<Unit::DynamicViscosity>(value, unit) {}
 
-  DynamicViscosity(double value, Unit::DynamicViscosity unit) noexcept : DimensionalScalarQuantity<Unit::DynamicViscosity>(value, unit) {}
+  constexpr DynamicViscosity(const KinematicViscosity& kinematic_viscosity,
+                             const MassDensity& mass_density) noexcept
+      : DynamicViscosity(kinematic_viscosity.Value() * mass_density.Value()) {}
 
-  constexpr DynamicViscosity(const KinematicViscosity& kinematic_viscosity, const MassDensity& mass_density) noexcept : DynamicViscosity(kinematic_viscosity.Value() * mass_density.Value()) {}
+  constexpr DynamicViscosity(const ReynoldsNumber& reynolds_number,
+                             const MassDensity& mass_density,
+                             const Speed& speed, const Length& length) noexcept;
 
-  constexpr DynamicViscosity(const ReynoldsNumber& reynolds_number, const MassDensity& mass_density, const Speed& speed, const Length& length) noexcept;
+  constexpr DynamicViscosity(
+      const PrandtlNumber& prandtl_number,
+      const ThermalConductivityScalar& thermal_conductivity_scalar,
+      SpecificIsobaricHeatCapacity& specific_isobaric_heat_capacity) noexcept;
 
-  constexpr DynamicViscosity(const PrandtlNumber& prandtl_number, const ThermalConductivityScalar& thermal_conductivity_scalar, SpecificIsobaricHeatCapacity& specific_isobaric_heat_capacity) noexcept;
-
-  constexpr bool operator==(const DynamicViscosity& dynamic_viscosity) const noexcept {
-    return value_ == dynamic_viscosity.value_;
+  inline DynamicViscosity operator+(
+      const DynamicViscosity& dynamic_viscosity) const noexcept {
+    return DynamicViscosity{value_ + dynamic_viscosity.value_};
   }
 
-  constexpr bool operator!=(const DynamicViscosity& dynamic_viscosity) const noexcept {
-    return value_ != dynamic_viscosity.value_;
+  inline DynamicViscosity operator-(
+      const DynamicViscosity& dynamic_viscosity) const noexcept {
+    return DynamicViscosity{value_ - dynamic_viscosity.value_};
   }
 
-  constexpr bool operator<(const DynamicViscosity& dynamic_viscosity) const noexcept {
-    return value_ < dynamic_viscosity.value_;
+  inline DynamicViscosity operator*(const double number) const noexcept {
+    return DynamicViscosity{value_ * number};
   }
 
-  constexpr bool operator<=(const DynamicViscosity& dynamic_viscosity) const noexcept {
-    return value_ <= dynamic_viscosity.value_;
+  inline DynamicViscosity operator/(const double number) const noexcept {
+    return DynamicViscosity{value_ / number};
   }
 
-  constexpr bool operator>(const DynamicViscosity& dynamic_viscosity) const noexcept {
-    return value_ > dynamic_viscosity.value_;
-  }
-
-  constexpr bool operator>=(const DynamicViscosity& dynamic_viscosity) const noexcept {
-    return value_ >= dynamic_viscosity.value_;
-  }
-
-  DynamicViscosity operator+(const DynamicViscosity& dynamic_viscosity) const noexcept {
-    return {value_ + dynamic_viscosity.value_};
-  }
-
-  constexpr void operator+=(const DynamicViscosity& dynamic_viscosity) noexcept {
-    value_ += dynamic_viscosity.value_;
-  }
-
-  DynamicViscosity operator-(const DynamicViscosity& dynamic_viscosity) const noexcept {
-    return {value_ - dynamic_viscosity.value_};
-  }
-
-  constexpr void operator-=(const DynamicViscosity& dynamic_viscosity) noexcept {
-    value_ -= dynamic_viscosity.value_;
-  }
-
-  KinematicViscosity operator/(const MassDensity& mass_density) const noexcept {
+  inline KinematicViscosity operator/(
+      const MassDensity& mass_density) const noexcept {
     return {*this, mass_density};
   }
 
-  MassDensity operator/(const KinematicViscosity& kinematic_viscosity) const noexcept {
+  inline MassDensity operator/(
+      const KinematicViscosity& kinematic_viscosity) const noexcept {
     return {*this, kinematic_viscosity};
   }
 
+  inline constexpr void operator+=(
+      const DynamicViscosity& dynamic_viscosity) noexcept {
+    value_ += dynamic_viscosity.value_;
+  }
+
+  inline constexpr void operator-=(
+      const DynamicViscosity& dynamic_viscosity) noexcept {
+    value_ -= dynamic_viscosity.value_;
+  }
+
+  inline constexpr void operator*=(const double number) noexcept {
+    value_ *= number;
+  }
+
+  inline constexpr void operator/=(const double number) noexcept {
+    value_ /= number;
+  }
+
 private:
-
-  constexpr DynamicViscosity(double value) noexcept : DimensionalScalarQuantity<Unit::DynamicViscosity>(value) {}
-
+  explicit constexpr DynamicViscosity(const double value) noexcept
+      : DimensionalScalarQuantity<Unit::DynamicViscosity>(value) {}
 };
 
-constexpr MassDensity::MassDensity(const DynamicViscosity& dynamic_viscosity, const KinematicViscosity& kinematic_viscosity) noexcept : MassDensity(dynamic_viscosity.Value() / kinematic_viscosity.Value()) {}
+inline constexpr bool operator==(const DynamicViscosity& left,
+                                 const DynamicViscosity& right) noexcept {
+  return left.Value() == right.Value();
+}
 
-constexpr KinematicViscosity::KinematicViscosity(const DynamicViscosity& dynamic_viscosity, const MassDensity& mass_density) noexcept : KinematicViscosity(dynamic_viscosity.Value() / mass_density.Value()) {}
+inline constexpr bool operator!=(const DynamicViscosity& left,
+                                 const DynamicViscosity& right) noexcept {
+  return left.Value() != right.Value();
+}
 
-DynamicViscosity KinematicViscosity::operator*(const MassDensity& mass_density) const noexcept {
+inline constexpr bool operator<(const DynamicViscosity& left,
+                                const DynamicViscosity& right) noexcept {
+  return left.Value() < right.Value();
+}
+
+inline constexpr bool operator>(const DynamicViscosity& left,
+                                const DynamicViscosity& right) noexcept {
+  return left.Value() > right.Value();
+}
+
+inline constexpr bool operator<=(const DynamicViscosity& left,
+                                 const DynamicViscosity& right) noexcept {
+  return left.Value() <= right.Value();
+}
+
+inline constexpr bool operator>=(const DynamicViscosity& left,
+                                 const DynamicViscosity& right) noexcept {
+  return left.Value() >= right.Value();
+}
+
+inline std::ostream& operator<<(
+    std::ostream& stream, const DynamicViscosity& dynamic_viscosity) noexcept {
+  stream << dynamic_viscosity.Print();
+  return stream;
+}
+
+inline DynamicViscosity operator*(
+    const double number, const DynamicViscosity& dynamic_viscosity) noexcept {
+  return dynamic_viscosity * number;
+}
+
+inline constexpr MassDensity::MassDensity(
+    const DynamicViscosity& dynamic_viscosity,
+    const KinematicViscosity& kinematic_viscosity) noexcept
+    : MassDensity(dynamic_viscosity.Value() / kinematic_viscosity.Value()) {}
+
+inline constexpr KinematicViscosity::KinematicViscosity(
+    const DynamicViscosity& dynamic_viscosity,
+    const MassDensity& mass_density) noexcept
+    : KinematicViscosity(dynamic_viscosity.Value() / mass_density.Value()) {}
+
+inline DynamicViscosity KinematicViscosity::operator*(
+    const MassDensity& mass_density) const noexcept {
   return {*this, mass_density};
 }
 
-DynamicViscosity MassDensity::operator*(const KinematicViscosity& kinematic_viscosity) const noexcept {
+inline DynamicViscosity MassDensity::operator*(
+    const KinematicViscosity& kinematic_viscosity) const noexcept {
   return {kinematic_viscosity, *this};
 }
 
-} // namespace PhQ
+}  // namespace PhQ
 
 namespace std {
 
-template <> struct hash<PhQ::DynamicViscosity> {
+template <>
+struct hash<PhQ::DynamicViscosity> {
   size_t operator()(const PhQ::DynamicViscosity& dynamic_viscosity) const {
     return hash<double>()(dynamic_viscosity.Value());
   }
 };
 
-} // namespace std
+}  // namespace std
+
+#endif  // PHYSICAL_QUANTITIES_INCLUDE_PHQ_DYNAMIC_VISCOSITY_HPP
---
+++ test/DynamicViscosity.cpp
@@ -0,0 +1,157 @@
+// Copyright 2020-2023 Alexandre Coderre-Chabot
+//
+// This file is part of Physical Quantities (PhQ), a C++ library of physical
+// quantities, physical models, and units of measure for scientific computation.
+//
+// Physical Quantities is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version. Physical Quantities is distributed in the hope
+// that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details. You should have received a
+// copy of the GNU Lesser General Public License along with Physical Quantities.
+// If not, see <https://www.gnu.org/licenses/>.
+
+#include "../include/PhQ/DynamicViscosity.hpp"
+
+#include <gtest/gtest.h>
+
+#include <unordered_set>
+
+namespace PhQ {
+
+namespace {
+
+TEST(DynamicViscosity, Accessor) {
+  const DynamicViscosity viscosity{0.1, Unit::DynamicViscosity::PascalSecond};
+  EXPECT_DOUBLE_EQ(viscosity.Value(), 0.1);
+  EXPECT_DOUBLE_EQ(viscosity.Value(Unit::DynamicViscosity::KilopascalSecond),
+                   0.1 * 0.001);
+}
+
+TEST(DynamicViscosity, Arithmetic) {
+  const KinematicViscosity kinematic{2.0,
+                                     Unit::Diffusivity::SquareMetrePerSecond};
+  const MassDensity density{0.5, Unit::MassDensity::KilogramPerCubicMetre};
+  const DynamicViscosity viscosity0{0.0, Unit::DynamicViscosity::PascalSecond};
+  const DynamicViscosity viscosity1{1.0, Unit::DynamicViscosity::PascalSecond};
+  const DynamicViscosity viscosity2{2.0, Unit::DynamicViscosity::PascalSecond};
+  EXPECT_EQ(viscosity1 + viscosity1, viscosity2);
+  EXPECT_EQ(viscosity1 - viscosity1, viscosity0);
+  EXPECT_EQ(viscosity1 * 2.0, viscosity2);
+  EXPECT_EQ(2.0 * viscosity1, viscosity2);
+  EXPECT_EQ(kinematic * density, viscosity1);
+  EXPECT_EQ(density * kinematic, viscosity1);
+  EXPECT_EQ(viscosity2 / 2.0, viscosity1);
+  EXPECT_EQ(viscosity1 / density, kinematic);
+  EXPECT_EQ(viscosity1 / kinematic, density);
+
+  DynamicViscosity viscosity3{viscosity1};
+  viscosity3 += viscosity1;
+  EXPECT_EQ(viscosity3, viscosity2);
+
+  DynamicViscosity viscosity4{viscosity1};
+  viscosity4 -= viscosity1;
+  EXPECT_EQ(viscosity4, viscosity0);
+
+  DynamicViscosity viscosity5{viscosity1};
+  viscosity5 *= 2.0;
+  EXPECT_EQ(viscosity5, viscosity2);
+
+  DynamicViscosity viscosity6{viscosity2};
+  viscosity6 /= 2.0;
+  EXPECT_EQ(viscosity6, viscosity1);
+}
+
+TEST(DynamicViscosity, Comparison) {
+  const DynamicViscosity viscosity0{0.1, Unit::DynamicViscosity::PascalSecond};
+  const DynamicViscosity viscosity1{0.2, Unit::DynamicViscosity::PascalSecond};
+  EXPECT_EQ(viscosity0, viscosity0);
+  EXPECT_NE(viscosity0, viscosity1);
+  EXPECT_LT(viscosity0, viscosity1);
+  EXPECT_GT(viscosity1, viscosity0);
+  EXPECT_LE(viscosity0, viscosity0);
+  EXPECT_LE(viscosity0, viscosity1);
+  EXPECT_GE(viscosity0, viscosity0);
+  EXPECT_GE(viscosity1, viscosity0);
+}
+
+TEST(DynamicViscosity, Constructor) {
+  const KinematicViscosity kinematic0{2.0,
+                                      Unit::Diffusivity::SquareMetrePerSecond};
+  const MassDensity density0{0.5, Unit::MassDensity::KilogramPerCubicMetre};
+  const DynamicViscosity dynamic0{kinematic0, density0};
+  const KinematicViscosity kinematic1{dynamic0, density0};
+  const MassDensity density1{dynamic0, kinematic1};
+  const DynamicViscosity dynamic1;
+  const DynamicViscosity dynamic2{100.0,
+                                  Unit::DynamicViscosity::KilopascalSecond};
+}
+
+TEST(DynamicViscosity, Hash) {
+  const DynamicViscosity viscosity0{10.0,
+                                    Unit::DynamicViscosity::KilopascalSecond};
+  const DynamicViscosity viscosity1{10.000001,
+                                    Unit::DynamicViscosity::KilopascalSecond};
+  const DynamicViscosity viscosity2{11.0,
+                                    Unit::DynamicViscosity::KilopascalSecond};
+  const DynamicViscosity viscosity3{-10.0,
+                                    Unit::DynamicViscosity::KilopascalSecond};
+  const DynamicViscosity viscosity4{20000.0,
+                                    Unit::DynamicViscosity::KilopascalSecond};
+  const DynamicViscosity viscosity5{-123.456,
+                                    Unit::DynamicViscosity::KilopascalSecond};
+  const std::hash<DynamicViscosity> hasher;
+  EXPECT_NE(hasher(viscosity0), hasher(viscosity1));
+  EXPECT_NE(hasher(viscosity0), hasher(viscosity2));
+  EXPECT_NE(hasher(viscosity0), hasher(viscosity3));
+  EXPECT_NE(hasher(viscosity0), hasher(viscosity4));
+  EXPECT_NE(hasher(viscosity0), hasher(viscosity5));
+  const std::unordered_set<DynamicViscosity> unordered{
+      viscosity0, viscosity1, viscosity2, viscosity3, viscosity4, viscosity5};
+}
+
+TEST(DynamicViscosity, Json) {
+  EXPECT_EQ(DynamicViscosity(1.11, Unit::DynamicViscosity::PascalSecond).Json(),
+            "{\"value\":1.110000,\"unit\":\"Pa·s\"}");
+  EXPECT_EQ(DynamicViscosity(-5.0, Unit::DynamicViscosity::KilopascalSecond)
+                .Json(Unit::DynamicViscosity::KilopascalSecond),
+            "{\"value\":-5.000000,\"unit\":\"kPa·s\"}");
+}
+
+TEST(DynamicViscosity, Print) {
+  EXPECT_EQ(
+      DynamicViscosity(1.11, Unit::DynamicViscosity::PascalSecond).Print(),
+      "1.110000 Pa·s");
+  EXPECT_EQ(DynamicViscosity(-5.0, Unit::DynamicViscosity::KilopascalSecond)
+                .Print(Unit::DynamicViscosity::KilopascalSecond),
+            "-5.000000 kPa·s");
+}
+
+TEST(DynamicViscosity, Stream) {
+  const DynamicViscosity viscosity{1.11, Unit::DynamicViscosity::PascalSecond};
+  std::ostringstream stream;
+  stream << viscosity;
+  EXPECT_EQ(stream.str(), viscosity.Print());
+}
+
+TEST(DynamicViscosity, Xml) {
+  EXPECT_EQ(DynamicViscosity(1.11, Unit::DynamicViscosity::PascalSecond).Xml(),
+            "<value>1.110000</value><unit>Pa·s</unit>");
+  EXPECT_EQ(DynamicViscosity(-5.0, Unit::DynamicViscosity::KilopascalSecond)
+                .Xml(Unit::DynamicViscosity::KilopascalSecond),
+            "<value>-5.000000</value><unit>kPa·s</unit>");
+}
+
+TEST(DynamicViscosity, Yaml) {
+  EXPECT_EQ(DynamicViscosity(1.11, Unit::DynamicViscosity::PascalSecond).Yaml(),
+            "{value:1.110000,unit:\"Pa·s\"}");
+  EXPECT_EQ(DynamicViscosity(-5.0, Unit::DynamicViscosity::KilopascalSecond)
+                .Yaml(Unit::DynamicViscosity::KilopascalSecond),
+            "{value:-5.000000,unit:\"kPa·s\"}");
+}
+
+}  // namespace
+
+}  // namespace PhQ
