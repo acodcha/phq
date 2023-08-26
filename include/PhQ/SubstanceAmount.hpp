04310c020dee95297ef9dca25239708a99ed9301
Alexandre Coderre-Chabot <alexandre.coderre.chabot@gmail.com>
2023-08-25 22:31:55 -0700

    Implement amount of substance quantity.

---
+++ include/PhQ/SubstanceAmount.hpp
@@ -0,0 +1,138 @@
+// Copyright 2020-2023 Alexandre Coderre-Chabot
+//
+// This file is part of Physical Quantities (PhQ), a C++ library of physical
+// quantities, physical models, and units of measure for scientific computation.
+//
+// Physical Quantities is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version. Physical Quantities is distributed in the hope
+// that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details. You should have received a
+// copy of the GNU Lesser General Public License along with Physical Quantities.
+// If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef PHYSICAL_QUANTITIES_INCLUDE_PHQ_SUBSTANCE_AMOUNT_HPP
+#define PHYSICAL_QUANTITIES_INCLUDE_PHQ_SUBSTANCE_AMOUNT_HPP
+
+#include "DimensionalScalarQuantity.hpp"
+#include "Unit/SubstanceAmount.hpp"
+
+namespace PhQ {
+
+// Amount of substance scalar quantity.
+class SubstanceAmount
+  : public DimensionalScalarQuantity<Unit::SubstanceAmount> {
+public:
+  constexpr SubstanceAmount() noexcept
+    : DimensionalScalarQuantity<Unit::SubstanceAmount>() {}
+
+  SubstanceAmount(const double value, const Unit::SubstanceAmount unit) noexcept
+    : DimensionalScalarQuantity<Unit::SubstanceAmount>(value, unit) {}
+
+  static constexpr SubstanceAmount Zero() noexcept {
+    return SubstanceAmount{0.0};
+  }
+
+  template<Unit::SubstanceAmount Unit>
+  static constexpr SubstanceAmount Create(const double value) noexcept {
+    return SubstanceAmount{
+        StaticConvertCopy<Unit::SubstanceAmount, Unit,
+                          Standard<Unit::SubstanceAmount>>(value)};
+  }
+
+  constexpr SubstanceAmount operator+(
+      const SubstanceAmount& substance_amount) const noexcept {
+    return SubstanceAmount{value_ + substance_amount.value_};
+  }
+
+  constexpr SubstanceAmount operator-(
+      const SubstanceAmount& substance_amount) const noexcept {
+    return SubstanceAmount{value_ - substance_amount.value_};
+  }
+
+  constexpr SubstanceAmount operator*(const double number) const noexcept {
+    return SubstanceAmount{value_ * number};
+  }
+
+  constexpr SubstanceAmount operator/(const double number) const noexcept {
+    return SubstanceAmount{value_ / number};
+  }
+
+  constexpr double operator/(
+      const SubstanceAmount& substance_amount) const noexcept {
+    return value_ / substance_amount.value_;
+  }
+
+  constexpr void operator+=(const SubstanceAmount& substance_amount) noexcept {
+    value_ += substance_amount.value_;
+  }
+
+  constexpr void operator-=(const SubstanceAmount& substance_amount) noexcept {
+    value_ -= substance_amount.value_;
+  }
+
+  constexpr void operator*=(const double number) noexcept { value_ *= number; }
+
+  constexpr void operator/=(const double number) noexcept { value_ /= number; }
+
+private:
+  explicit constexpr SubstanceAmount(const double value) noexcept
+    : DimensionalScalarQuantity<Unit::SubstanceAmount>(value) {}
+};
+
+inline constexpr bool operator==(
+    const SubstanceAmount& left, const SubstanceAmount& right) noexcept {
+  return left.Value() == right.Value();
+}
+
+inline constexpr bool operator!=(
+    const SubstanceAmount& left, const SubstanceAmount& right) noexcept {
+  return left.Value() != right.Value();
+}
+
+inline constexpr bool operator<(
+    const SubstanceAmount& left, const SubstanceAmount& right) noexcept {
+  return left.Value() < right.Value();
+}
+
+inline constexpr bool operator>(
+    const SubstanceAmount& left, const SubstanceAmount& right) noexcept {
+  return left.Value() > right.Value();
+}
+
+inline constexpr bool operator<=(
+    const SubstanceAmount& left, const SubstanceAmount& right) noexcept {
+  return left.Value() <= right.Value();
+}
+
+inline constexpr bool operator>=(
+    const SubstanceAmount& left, const SubstanceAmount& right) noexcept {
+  return left.Value() >= right.Value();
+}
+
+inline std::ostream& operator<<(
+    std::ostream& stream, const SubstanceAmount& substance_amount) noexcept {
+  stream << substance_amount.Print();
+  return stream;
+}
+
+inline constexpr SubstanceAmount operator*(
+    const double number, const SubstanceAmount& substance_amount) noexcept {
+  return substance_amount * number;
+}
+
+}  // namespace PhQ
+
+namespace std {
+
+template<> struct hash<PhQ::SubstanceAmount> {
+  inline size_t operator()(const PhQ::SubstanceAmount& substance_amount) const {
+    return hash<double>()(substance_amount.Value());
+  }
+};
+
+}  // namespace std
+
+#endif  // PHYSICAL_QUANTITIES_INCLUDE_PHQ_SUBSTANCE_AMOUNT_HPP
---
+++ test/SubstanceAmount.cpp
@@ -0,0 +1,159 @@
+// Copyright 2020-2023 Alexandre Coderre-Chabot
+//
+// This file is part of Physical Quantities (PhQ), a C++ library of physical
+// quantities, physical models, and units of measure for scientific computation.
+//
+// Physical Quantities is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version. Physical Quantities is distributed in the hope
+// that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details. You should have received a
+// copy of the GNU Lesser General Public License along with Physical Quantities.
+// If not, see <https://www.gnu.org/licenses/>.
+
+#include "../include/PhQ/SubstanceAmount.hpp"
+
+#include <gtest/gtest.h>
+
+#include <unordered_set>
+
+namespace PhQ {
+
+namespace {
+
+TEST(SubstanceAmount, Accessor) {
+  const SubstanceAmount substance_amount{0.1, Unit::SubstanceAmount::Mole};
+  EXPECT_DOUBLE_EQ(substance_amount.Value(), 0.1);
+  EXPECT_DOUBLE_EQ(
+      substance_amount.Value(Unit::SubstanceAmount::Kilomole), 0.1 * 0.001);
+}
+
+TEST(SubstanceAmount, Arithmetic) {
+  const SubstanceAmount substance_amount_0{1.0, Unit::SubstanceAmount::Mole};
+  EXPECT_EQ(substance_amount_0 + substance_amount_0,
+            SubstanceAmount(2.0, Unit::SubstanceAmount::Mole));
+  EXPECT_EQ(substance_amount_0 - substance_amount_0,
+            SubstanceAmount(0.0, Unit::SubstanceAmount::Mole));
+  EXPECT_EQ(substance_amount_0 * 2.0,
+            SubstanceAmount(2.0, Unit::SubstanceAmount::Mole));
+  EXPECT_EQ(2.0 * substance_amount_0,
+            SubstanceAmount(2.0, Unit::SubstanceAmount::Mole));
+  EXPECT_EQ(substance_amount_0 / 2.0,
+            SubstanceAmount(0.5, Unit::SubstanceAmount::Mole));
+  EXPECT_EQ(substance_amount_0 / substance_amount_0, 1.0);
+
+  SubstanceAmount substance_amount_1{1.0, Unit::SubstanceAmount::Mole};
+  substance_amount_1 += SubstanceAmount{1.0, Unit::SubstanceAmount::Mole};
+  EXPECT_EQ(
+      substance_amount_1, SubstanceAmount(2.0, Unit::SubstanceAmount::Mole));
+
+  SubstanceAmount substance_amount_2{2.0, Unit::SubstanceAmount::Mole};
+  substance_amount_2 -= SubstanceAmount{1.0, Unit::SubstanceAmount::Mole};
+  EXPECT_EQ(
+      substance_amount_2, SubstanceAmount(1.0, Unit::SubstanceAmount::Mole));
+
+  SubstanceAmount substance_amount_3{1.0, Unit::SubstanceAmount::Mole};
+  substance_amount_3 *= 2.0;
+  EXPECT_EQ(
+      substance_amount_3, SubstanceAmount(2.0, Unit::SubstanceAmount::Mole));
+
+  SubstanceAmount substance_amount_4{2.0, Unit::SubstanceAmount::Mole};
+  substance_amount_4 /= 2.0;
+  EXPECT_EQ(
+      substance_amount_4, SubstanceAmount(1.0, Unit::SubstanceAmount::Mole));
+}
+
+TEST(SubstanceAmount, Comparison) {
+  const SubstanceAmount substance_amount_0{0.1, Unit::SubstanceAmount::Mole};
+  const SubstanceAmount substance_amount_1{0.2, Unit::SubstanceAmount::Mole};
+  EXPECT_EQ(substance_amount_0, substance_amount_0);
+  EXPECT_NE(substance_amount_0, substance_amount_1);
+  EXPECT_LT(substance_amount_0, substance_amount_1);
+  EXPECT_GT(substance_amount_1, substance_amount_0);
+  EXPECT_LE(substance_amount_0, substance_amount_0);
+  EXPECT_LE(substance_amount_0, substance_amount_1);
+  EXPECT_GE(substance_amount_0, substance_amount_0);
+  EXPECT_GE(substance_amount_1, substance_amount_0);
+}
+
+TEST(SubstanceAmount, Constructor) {
+  constexpr SubstanceAmount substance_amount_0;
+  const SubstanceAmount substance_amount_1{
+      2.0, Unit::SubstanceAmount::Kilomole};
+  constexpr SubstanceAmount substance_amount_2{
+      SubstanceAmount::Create<Unit::SubstanceAmount::Kilomole>(4.0)};
+}
+
+TEST(SubstanceAmount, Hash) {
+  const SubstanceAmount substance_amount_0{
+      10.0, Unit::SubstanceAmount::Kilomole};
+  const SubstanceAmount substance_amount_1{
+      10.000001, Unit::SubstanceAmount::Kilomole};
+  const SubstanceAmount substance_amount_2{
+      11.0, Unit::SubstanceAmount::Kilomole};
+  const SubstanceAmount substance_amount_3{
+      -10.0, Unit::SubstanceAmount::Kilomole};
+  const SubstanceAmount substance_amount_4{
+      20000.0, Unit::SubstanceAmount::Kilomole};
+  const SubstanceAmount substance_amount_5{
+      -123.456, Unit::SubstanceAmount::Kilomole};
+  const std::hash<SubstanceAmount> hasher;
+  EXPECT_NE(hasher(substance_amount_0), hasher(substance_amount_1));
+  EXPECT_NE(hasher(substance_amount_0), hasher(substance_amount_2));
+  EXPECT_NE(hasher(substance_amount_0), hasher(substance_amount_3));
+  EXPECT_NE(hasher(substance_amount_0), hasher(substance_amount_4));
+  EXPECT_NE(hasher(substance_amount_0), hasher(substance_amount_5));
+  const std::unordered_set<SubstanceAmount> unordered{
+      substance_amount_0, substance_amount_1, substance_amount_2,
+      substance_amount_3, substance_amount_4, substance_amount_5};
+}
+
+TEST(SubstanceAmount, JSON) {
+  EXPECT_EQ(SubstanceAmount(1.11, Unit::SubstanceAmount::Mole).JSON(),
+            "{\"value\":1.110000000000000,\"unit\":\"mol\"}");
+  EXPECT_EQ(SubstanceAmount(-5.0, Unit::SubstanceAmount::Kilomole)
+                .JSON(Unit::SubstanceAmount::Kilomole),
+            "{\"value\":-5.000000000000000,\"unit\":\"kmol\"}");
+}
+
+TEST(SubstanceAmount, Print) {
+  EXPECT_EQ(SubstanceAmount(1.11, Unit::SubstanceAmount::Mole).Print(),
+            "1.110000000000000 mol");
+  EXPECT_EQ(SubstanceAmount(-5.0, Unit::SubstanceAmount::Kilomole)
+                .Print(Unit::SubstanceAmount::Kilomole),
+            "-5.000000000000000 kmol");
+}
+
+TEST(SubstanceAmount, Stream) {
+  const SubstanceAmount substance_amount{1.11, Unit::SubstanceAmount::Mole};
+  std::ostringstream stream;
+  stream << substance_amount;
+  EXPECT_EQ(stream.str(), substance_amount.Print());
+}
+
+TEST(SubstanceAmount, XML) {
+  EXPECT_EQ(SubstanceAmount(1.11, Unit::SubstanceAmount::Mole).XML(),
+            "<value>1.110000000000000</value><unit>mol</unit>");
+  EXPECT_EQ(SubstanceAmount(-5.0, Unit::SubstanceAmount::Kilomole)
+                .XML(Unit::SubstanceAmount::Kilomole),
+            "<value>-5.000000000000000</value><unit>kmol</unit>");
+}
+
+TEST(SubstanceAmount, YAML) {
+  EXPECT_EQ(SubstanceAmount(1.11, Unit::SubstanceAmount::Mole).YAML(),
+            "{value:1.110000000000000,unit:\"mol\"}");
+  EXPECT_EQ(SubstanceAmount(-5.0, Unit::SubstanceAmount::Kilomole)
+                .YAML(Unit::SubstanceAmount::Kilomole),
+            "{value:-5.000000000000000,unit:\"kmol\"}");
+}
+
+TEST(SubstanceAmount, Zero) {
+  EXPECT_EQ(SubstanceAmount::Zero(),
+            SubstanceAmount(0.0, Unit::SubstanceAmount::Mole));
+}
+
+}  // namespace
+
+}  // namespace PhQ
