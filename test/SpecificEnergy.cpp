20c56366f63edca69a6aee1cf9e5d709766932bd
Alexandre Coderre-Chabot <alexandre.coderre.chabot@gmail.com>
2024-05-13 23:06:35 -0700

    Update license

--- test/SpecificEnergy.cpp
+++ test/SpecificEnergy.cpp
@@ -1,15 +1,28 @@
-// Copyright 2020-2024 Alexandre Coderre-Chabot
+// Copyright © 2020-2024 Alexandre Coderre-Chabot
 //
-// Physical Quantities (PhQ): A C++ library of physical quantities, physical models, and units of
-// measure for scientific computation. https://github.com/acodcha/physical-quantities
+// This file is part of Physical Quantities (PhQ), a C++ library of physical quantities, physical
+// models, and units of measure for scientific computation.
 //
-// Physical Quantities (PhQ) is free software: you can redistribute it and/or modify it under the
-// terms of the GNU Lesser General Public License as published by the Free Software Foundation,
-// either version 3 of the License, or (at your option) any later version. Physical Quantities (PhQ)
-// is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
-// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser
-// General Public License for more details. You should have received a copy of the GNU Lesser
-// General Public License along with Physical Quantities (PhQ). https://www.gnu.org/licenses
+// Physical Quantities (PhQ) is hosted at:
+//     https://github.com/acodcha/physical-quantities
+//
+// Physical Quantities (PhQ) is licensed under the MIT License:
+//     https://mit-license.org
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+// associated documentation files (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge, publish, distribute,
+// sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+// NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM OUT
+// OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #include "../include/PhQ/SpecificEnergy.hpp"
 
