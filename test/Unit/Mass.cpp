80c065cc41286645ae7a6715f52ee7bd4ca76647
Alexandre Coderre-Chabot <alexandre.coderre.chabot@gmail.com>
2024-06-01 14:27:29 -0700

    Update documentation

--- test/Unit/Mass.cpp
+++ test/Unit/Mass.cpp
@@ -3,10 +3,10 @@
 // This file is part of Physical Quantities (PhQ), a C++ library of physical quantities, physical
 // models, and units of measure for scientific computing.
 //
-// Physical Quantities (PhQ) is hosted at:
+// Physical Quantities is hosted at:
 //     https://github.com/acodcha/phq
 //
-// Physical Quantities (PhQ) is licensed under the MIT License:
+// Physical Quantities is licensed under the MIT License:
 //     https://mit-license.org
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
@@ -14,15 +14,13 @@
 // including without limitation the rights to use, copy, modify, merge, publish, distribute,
 // sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all copies or
-// substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
-// NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM OUT
-// OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//   - The above copyright notice and this permission notice shall be included in all copies or
+//     substantial portions of the Software.
+//   - THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+//     BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//     NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+//     DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM
+//     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #include "../../include/PhQ/Unit/Mass.hpp"
 
